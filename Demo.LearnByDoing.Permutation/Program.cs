33b0549e6f8ce57c49d5d026e91d89b9937bb00d
dance2die <dontbothermewithspam@gmail.com>
2016-11-25 15:32:14 -0500

    Permutation Test of strings

---
+++ Demo.LearnByDoing.Permutation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Demo.LearnByDoing.Permutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Permutation("abc");
+        }
+
+        private static void Permutation(string str, string prefix = "")
+        {
+            if (str.Length == 0)
+            {
+                Console.WriteLine(prefix);
+            }
+            else
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    string remainder = str.Substring(0, i) + str.Substring(i + 1);
+                    Permutation(remainder, prefix + str[i]);
+                }
+            }
+        }
+    }
+}
