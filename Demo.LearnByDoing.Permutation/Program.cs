2b02e4b709bbdb33e679616b0aafa3fd4c50de4c
dance2die <dontbothermewithspam@gmail.com>
2016-11-25 16:03:10 -0500

    Permutation test

--- Demo.LearnByDoing.Permutation/Program.cs
+++ Demo.LearnByDoing.Permutation/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Demo.LearnByDoing.Permutation
 {
@@ -6,7 +8,45 @@ namespace Demo.LearnByDoing.Permutation
     {
         static void Main(string[] args)
         {
-            Permutation("abc");
+            var input = "abc";
+            //Permutation(input);
+
+            //Console.WriteLine("++++++++++");
+
+            IEnumerable<string> permutations = GetPermutations(input).ToList();
+            foreach (var permutation in permutations)
+            {
+                Console.WriteLine(permutation);
+            }
+        }
+
+        private static IEnumerable<string> GetPermutations(string str)
+        {
+            return GetPermutations(str, "");
+        }
+
+        private static IEnumerable<string> GetPermutations(string str, string prefix)
+        {
+            Console.WriteLine("str:prefix => {0}:{1} ", str, prefix);
+
+            if (str.Length == 0)
+            {
+                Console.WriteLine("\tstr.Length == 0: prefix: '{0}'", prefix);
+                yield return prefix;
+            }
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                string remainder = str.Substring(0, i) + str.Substring(i + 1);
+                //Console.WriteLine("'{0}' => remainder: {1}", i, remainder);
+                //Console.WriteLine("\tprefix:str.Substring(0, i) => str.Substring(i + 1) {0}:{1}{2}", prefix, str.Substring(0, i), str.Substring(i + 1));
+
+                foreach (var permutation in GetPermutations(remainder, prefix + str[i]).ToList())
+                {
+                    //Console.WriteLine("\tpermutation: {0}", permutation);
+                    yield return permutation;
+                }
+            }
         }
 
         private static void Permutation(string str, string prefix = "")
