693cde25f7c6d4178529c1e81c4696b275f74c28
dance2die <dontbothermewithspam@gmail.com>
2016-12-05 20:21:27 -0500

    Solved Alone: Question 1.2

--- Demo.LearnByDoing.Tests/Chapter01/Chapter1_2Test.cs
+++ Demo.LearnByDoing.Tests/Chapter01/Chapter1_2Test.cs
@@ -1,4 +1,6 @@
-﻿using Demo.LearnByDoing.Tests.Core;
+﻿using System;
+using Demo.LearnByDoing.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.LearnByDoing.Tests.Chapter01
@@ -14,10 +16,36 @@ namespace Demo.LearnByDoing.Tests.Chapter01
         {
         }
 
+        [Theory]
+        [InlineData("1", "1", true)]
+        [InlineData("abc", "bca", true)]
+        [InlineData("12345", "54321", true)]
+        [InlineData("cca", "ccc", false)]
+        [InlineData("hello world!", "!world hell", false)]
+        public void CompareIfOneTextIsAPermutationOfTheOther(string text1, string text2, bool expected)
+        {
+            bool actual = _sut.AreStringsPermutational(text1, text2);
 
+            Assert.Equal(expected, actual);
+        }
     }
 
     public class Chapter1_2
     {
+        public bool AreStringsPermutational(string text1, string text2)
+        {
+            string sortedText1 = SortAlphabetically(text1);
+            string sortedText2 = SortAlphabetically(text2);
+
+            return sortedText1 == sortedText2;
+        }
+
+        private string SortAlphabetically(string text)
+        {
+            var charArray = text.ToCharArray();
+            Array.Sort(charArray);
+
+            return new string(charArray);
+        }
     }
 }
