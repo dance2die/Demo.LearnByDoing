441c08d5ce240b4a8bb0d3eacd4dd0e7451febf7
dance2die <dontbothermewithspam@gmail.com>
2016-12-05 22:03:39 -0500

    Refactored classes to their own files

--- Demo.LearnByDoing.Tests/Chapter01/Chapter1_2Test.cs
+++ Demo.LearnByDoing.Tests/Chapter01/Chapter1_2Test.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using Demo.LearnByDoing.Tests.Core;
+﻿using Demo.LearnByDoing.Tests.Core;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -35,70 +31,6 @@ namespace Demo.LearnByDoing.Tests.Chapter01
             bool actual = _sut.AreStringsPermutationalUsingLinq(text1, text2);
 
             Assert.Equal(expected, actual);
-        }
-    }
-
-    public class Chapter1_2
-    {
-        public bool AreStringsPermutationalUsingLinq(string text1, string text2)
-        {
-            if (text1.Length != text2.Length) return false;
-
-            // Use "Join" to get a list of common elements between two lists.
-            // If two texts have the same characters, then the join count and the text will be the same.
-            var items = from first in text1.ToCharArray().ToList()
-                join second in text2.ToCharArray().ToList()
-                on first equals second
-                select first;
-
-            return items.Count() == text1.Length;
-        }
-
-        public bool AreStringsPermutational(string text1, string text2)
-        {
-            if (text1.Length != text2.Length) return false;
-
-            string sortedText1 = SortAlphabetically(text1);
-            string sortedText2 = SortAlphabetically(text2);
-
-            return sortedText1 == sortedText2;
-        }
-
-        private string SortAlphabetically(string text)
-        {
-            var charArray = text.ToCharArray();
-            Array.Sort(charArray);
-
-            return new string(charArray);
-        }
-    }
-
-    /// <summary>
-    /// Data for testing permutations
-    /// </summary>
-    /// <remarks>
-    /// http://stackoverflow.com/a/22093968/4035
-    /// </remarks>
-    public class Chapter1_2Data : IEnumerable<object[]>
-    {
-        private readonly List<object[]> _data = new List<object[]>
-        {
-            new object[] { "1", "1", true },
-            new object[] {"abc", "bca", true },
-            new object[] {"12345", "54321", true },
-            new object[] {"cca", "ccc", false },
-            new object[] {"a", "ab", false },
-            new object[] { "hello world!", "!world hell", false },
-        };
-
-        public IEnumerator<object[]> GetEnumerator()
-        {
-            return _data.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
         }
     }
 }
