a48632dfe8ae935cd64b2bc028ef900d9645a053
dance2die <dontbothermewithspam@gmail.com>
2016-12-06 21:02:02 -0500

    Question1.3: Refactored into separate files.

--- Demo.LearnByDoing.Tests/Chapter01/Chapter1_3Test.cs
+++ Demo.LearnByDoing.Tests/Chapter01/Chapter1_3Test.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using Demo.LearnByDoing.Tests.Core;
+﻿using Demo.LearnByDoing.Tests.Core;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -32,77 +29,6 @@ namespace Demo.LearnByDoing.Tests.Chapter01
             string actual = _sut.UrlifyString(text, trueLength);
 
             Assert.Equal(expected, actual);
-        }
-    }
-
-    public class Chapter1_3
-    {
-        public string UrlifyString(string text, int trueLength)
-        {
-            // Algorithm:
-            // Get substring "s" upto trueLength.
-            // Count the number of empty strings in "s"
-            // Create a new character array "result" where length = non-space character count + (space count * 3)
-
-            // foreach character "c" in "s"
-            // if "c" == space, then insert "%20" into "result" taking up three indexes.
-            // else insert "c" into "result".
-
-
-            char[] upto = text.Substring(0, trueLength).ToCharArray();
-
-            int spaceCount = upto.Where(c => c == ' ').Count();
-            int nonSpaceCount = upto.Length - spaceCount;
-
-            char[] encodedSpace = { '%', '2', '0' };
-            int resultLength = nonSpaceCount + (spaceCount * encodedSpace.Length);
-            char[] result = new char[resultLength];
-
-            // offset caused by adding "%20" instead of space.
-            int offset = 0;
-            for (int i = 0; i < upto.Length; i++)
-            {
-                var c = upto[i];
-
-                if (c == ' ')
-                {
-                    result[i + offset] = '%';
-                    result[i + 1 + offset] = '2';
-                    result[i + 2 + offset] = '0';
-
-                    offset += 2;
-                }
-                else
-                {
-                    result[i + offset] = c;
-                }
-            }
-
-            return new string(result);
-        }
-    }
-
-    public class Chapter1_3Data : IEnumerable<object[]>
-    {
-        private readonly List<object[]> _data = new List<object[]>
-        {
-            new object[] { "Mr John Smith    ", 13, "Mr%20John%20Smith" },
-            new object[] { "Mr  John  Smith    ", 15, "Mr%20%20John%20%20Smith" },
-            new object[] { "Hello World    ", 11, "Hello%20World" },
-            new object[] { "Hello World    ", 12, "Hello%20World%20" },
-            new object[] { "Hello World    ", 13, "Hello%20World%20%20" },
-            new object[] { " Hello World    ", 14, "%20Hello%20World%20%20" },
-            new object[] { " Hello  World    ", 13, "%20Hello%20%20World" },
-        };
-
-        public IEnumerator<object[]> GetEnumerator()
-        {
-            return _data.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
         }
     }
 }
