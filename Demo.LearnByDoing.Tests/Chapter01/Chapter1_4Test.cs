c91ac4d8941caa84df00790e0752805e254f37e7
dance2die <dontbothermewithspam@gmail.com>
2016-12-08 21:33:44 -0500

    Question 1.4: Refactored each class into separate files.

--- Demo.LearnByDoing.Tests/Chapter01/Chapter1_4Test.cs
+++ Demo.LearnByDoing.Tests/Chapter01/Chapter1_4Test.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using Demo.LearnByDoing.Tests.Core;
+﻿using Demo.LearnByDoing.Tests.Core;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -61,95 +56,5 @@ namespace Demo.LearnByDoing.Tests.Chapter01
         //{
         //    bool actual = _sut.
         //}
-    }
-
-    public class Chapter1_4
-    {
-        /// <summary>
-        /// Check if the text can have a palindrome sequence.
-        /// </summary>
-        public bool HasPalindrome(string text)
-        {
-            if (text.Length == 1) return true;
-            if (text.Length == 2 && text[0] == text[1]) return true;
-
-            // It's palindrome if there exists
-            // 1.) even number of characters for each character
-            // 2.) even number of characters except for one character.
-
-            // Test 1. even number of characters for each character
-            var grouped = text
-                .ToLower()
-                .ToCharArray()
-                .Where(IsNotSpace)
-                .GroupBy(c => c)
-                .Select(group => new 
-                {
-                    Character = group.Key,
-                    Count = group.Count()
-                })
-                .ToList();
-
-            Func<int, bool> isOdd = number => number % 2 == 1;
-
-            var totalOddCount = grouped.Count(group => isOdd(group.Count));
-            switch (totalOddCount)
-            {
-                case 0:
-                case 1:
-                    return true;
-                default:
-                    Debug.Assert(totalOddCount > 1);
-                    return false;
-            }
-        }
-
-        public bool IsPalindrome(string text)
-        {
-            // By definition, if one letter is a palindrome
-            if (text.Length == 1) return true;
-
-            string reverseWithoutSpace = new string(text.Where(IsNotSpace).Reverse().ToArray()).ToLower();
-            string textWithoutSpace = new string(text.Where(IsNotSpace).ToArray()).ToLower();
-
-            return textWithoutSpace == reverseWithoutSpace;
-        }
-
-        private bool IsNotSpace(char c)
-        {
-            return c != ' ';
-        }
-    }
-
-    public class Chapter1_4Data : IEnumerable<object[]>
-    {
-        private readonly List<object[]> _data = new List<object[]>
-        {
-            // Taco Cat or Atco Cta
-            new object[] {"Tact Coa", true},
-            new object[] {"aab", true},
-            new object[] {"aabbcc", true},
-            // Racecar
-            new object[] {"raccear", true},
-            // Kayak
-            new object[] {"yakka", true},
-            new object[] {"ybbakka", true},
-            new object[] {"abcab", true},
-            new object[] {"abbbc", false},
-            new object[] {"ab", false},
-            new object[] {"abc", false},
-            new object[] {"abcd", false},
-            new object[] {"abcde", false},
-        };
-
-        public IEnumerator<object[]> GetEnumerator()
-        {
-            return _data.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
     }
 }
