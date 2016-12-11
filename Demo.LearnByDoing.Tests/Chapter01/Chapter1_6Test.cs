6794128cc37b3e05d88c58624f90d918a6dd74bf
dance2die <dontbothermewithspam@gmail.com>
2016-12-11 13:41:13 -0500

    Question 1.6: Refactored classes to their own files.

--- Demo.LearnByDoing.Tests/Chapter01/Chapter1_6Test.cs
+++ Demo.LearnByDoing.Tests/Chapter01/Chapter1_6Test.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using Demo.LearnByDoing.Tests.Core;
+﻿using Demo.LearnByDoing.Tests.Core;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -29,87 +26,6 @@ namespace Demo.LearnByDoing.Tests.Chapter01
             string actual = _sut.CompressText(text);
 
             Assert.Equal(expected, actual);
-        }
-    }
-
-    public class Chapter1_6
-    {
-        public string CompressText(string text)
-        {
-            // To save time.
-            if (string.IsNullOrWhiteSpace(text) || text.Length == 1) return text;
-
-            string[] compressed = new string[text.Length];
-
-            // Set initial text to $prevChar
-            // for reach $currChar in $text
-            // if $prevChar == $currChar, then 
-            //      increase $charCount
-            // else 
-            //      Add $prevChar to $compressed with $charCount
-            //      Set $prevChar to $currChar
-            //      reset $charCount to 1
-            //
-            // After the loop is over, add the $prevChar to $compressed with $charCount
-            // return stringified $compressed
-
-            char prevChar = text[0];
-            int compressedIndex = 0;
-            int charCount = 1;
-            // we are adding two strings into array, current character and the number of occurrences.
-            const int indexOffset = 2;
-
-            // Skip the first character.
-            foreach (char currChar in text.Skip(1))
-            {
-                if (prevChar == currChar)
-                {
-                    charCount++;
-                }
-                else
-                {
-                    if (compressedIndex + indexOffset > text.Length) return text;
-
-                    compressed[compressedIndex] = prevChar.ToString();
-                    compressed[compressedIndex + 1] = charCount.ToString();
-
-                    prevChar = currChar;
-                    charCount = 1;
-
-                    compressedIndex += indexOffset;
-                }
-            }
-
-            // Set the last character's data
-            compressed[compressedIndex] = prevChar.ToString();
-            compressed[compressedIndex + 1] = charCount.ToString();
-
-            return string.Join("", compressed);
-        }
-    }
-
-    public class Chapter1_6Data : IEnumerable<object[]>
-    {
-        private readonly List<object[]> _data = new List<object[]>
-        {
-            new object[] { "aabcccccaaa", "a2b1c5a3" },
-            new object[] { "aaabcccccaaa", "a3b1c5a3" },
-            new object[] { "aaabcaaa", "a3b1c1a3" },
-            new object[] { "aabbcc", "a2b2c2" },
-            new object[] { "abcd", "abcd" },
-            new object[] { "a", "a" },
-            new object[] { "aa", "a2" },
-            new object[] { "", "" },
-        };
-
-        public IEnumerator<object[]> GetEnumerator()
-        {
-            return _data.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
         }
     }
 }
