b7709b4db756cc3ce06d504cc0badf0e9337f3e7
dance2die <dontbothermewithspam@gmail.com>
2016-12-11 13:49:46 -0500

    Question 1.7: Created a test class.

---
+++ Demo.LearnByDoing.Tests/Chapter01/Chapter1_7Test.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Demo.LearnByDoing.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.LearnByDoing.Tests.Chapter01
+{
+    /// <summary>
+    /// Rotate Matrix:
+    /// Given an image represented by an NxN matrix, 
+    /// where each piexel in the image is 4 bytes, 
+    /// write a method to rotate the image by 90 degrees. 
+    /// Can you do this in place?
+    /// </summary>
+    public class Chapter1_7Test : BaseTest
+    {
+        private readonly Chapter1_7 _sut = new Chapter1_7();
+
+        public Chapter1_7Test(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+
+    public class Chapter1_7
+    {
+    }
+
+    public class Chapter1_7Data : IEnumerable<object[]>
+    {
+        private readonly List<object[]> _data = new List<object[]>
+        {
+            
+        };
+
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            return _data.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
