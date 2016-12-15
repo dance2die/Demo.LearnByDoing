7994f98c3c5d671b9f957aa84e36a64c4aa71e35
dance2die <dontbothermewithspam@gmail.com>
2016-12-14 20:10:03 -0500

    Question 1.8: Created Chapter1_8Test

---
+++ Demo.LearnByDoing.Tests/Chapter01/Chapter1_8Test.cs
@@ -0,0 +1,22 @@
+﻿using Demo.LearnByDoing.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.LearnByDoing.Tests.Chapter01
+{
+    public class Chapter1_8Test : BaseTest
+    {
+        private readonly Chapter1_8 _sut = new Chapter1_8();
+
+        public Chapter1_8Test(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+
+    public class Chapter1_8
+    {
+    }
+
+    public class Chapter1_8Data
+    {
+    }
+}
