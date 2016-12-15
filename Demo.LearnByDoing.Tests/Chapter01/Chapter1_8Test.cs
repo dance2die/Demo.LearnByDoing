ad939ba1584545c273570fdc09f90758b0677451
dance2die <dontbothermewithspam@gmail.com>
2016-12-14 20:12:49 -0500

    Question 1.8: Added question comment

--- Demo.LearnByDoing.Tests/Chapter01/Chapter1_8Test.cs
+++ Demo.LearnByDoing.Tests/Chapter01/Chapter1_8Test.cs
@@ -3,6 +3,11 @@ using Xunit.Abstractions;
 
 namespace Demo.LearnByDoing.Tests.Chapter01
 {
+    /// <summary>
+    /// Zero Matrix:
+    /// Write an algorithm such that if an element in an MxN matrix is 0, 
+    /// its entire row and column are set to 0
+    /// </summary>
     public class Chapter1_8Test : BaseTest
     {
         private readonly Chapter1_8 _sut = new Chapter1_8();
