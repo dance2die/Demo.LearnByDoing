29290ecf7732c4acda2d742ccb46991f92430a51
dance2die <dontbothermewithspam@gmail.com>
2016-12-17 14:15:55 -0500

    Question 2.1: Created a test class.

---
+++ Demo.LearnByDoing.Tests/Chapter02/Chapter2_1Test.cs
@@ -0,0 +1,24 @@
+﻿using Demo.LearnByDoing.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.LearnByDoing.Tests.Chapter02
+{
+    /// <summary>
+    /// Remove Dups:
+    /// Write code to remove duplicates from an unsorted linked list.
+    /// FOLLOW UP
+    /// How would you solve this problem if a temporary buffer is not allowed?
+    /// </summary>
+    public class Chapter2_1Test : BaseTest
+    {
+        private readonly Chapter2_1 _sut = new Chapter2_1();
+
+        public Chapter2_1Test(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+
+    public class Chapter2_1
+    {
+    }
+}
