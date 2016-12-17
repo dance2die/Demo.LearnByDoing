bedef932b32c018257124422d36d9c5deb2dd138
dance2die <dontbothermewithspam@gmail.com>
2016-12-16 22:22:27 -0500

    Quesiton 1.9: Created a test class.

---
+++ Demo.LearnByDoing.Tests/Chapter01/Chapter1_9Test.cs
@@ -0,0 +1,24 @@
+﻿using Demo.LearnByDoing.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.LearnByDoing.Tests.Chapter01
+{
+    /// <summary>
+    /// String Rotation:
+    /// Assume you have a method isSubstring which checks if one word is a substring of another.
+    /// Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring
+    /// (e.g., "waterbottle" is a rotation of "erbottlewat").
+    /// </summary>
+    public class Chapter1_9Test : BaseTest
+    {
+        private readonly Chapter1_9 _sut = new Chapter1_9();
+
+        public Chapter1_9Test(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+
+    public class Chapter1_9
+    {
+    }
+}
