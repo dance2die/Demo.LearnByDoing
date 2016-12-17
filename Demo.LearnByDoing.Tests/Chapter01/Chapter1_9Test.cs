880d3cd4ffa769345b636426877d11a2c6f242b1
dance2die <dontbothermewithspam@gmail.com>
2016-12-17 08:39:05 -0500

    Question 1.9: Second implementation doesn't use IndexOf, which is just like subString.

--- Demo.LearnByDoing.Tests/Chapter01/Chapter1_9Test.cs
+++ Demo.LearnByDoing.Tests/Chapter01/Chapter1_9Test.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using Demo.LearnByDoing.Tests.Core;
 using Xunit;
 using Xunit.Abstractions;
@@ -16,7 +17,9 @@ namespace Demo.LearnByDoing.Tests.Chapter01
     /// <remarks>
     /// Sung's algorithm (cheated because I saw the hints):
     /// Combine S2 with itself: erbottlewat -> erbottlewaterbottlewat
-    /// Check if S1 is substring of S2.
+    /// for each character in s2 concatenated,
+    ///    up to the length of s1, check if each letter in s1 matches that in s2 concatenated.
+    ///    if it matches up to the length of s1, then return true, else false
     /// </remarks>
     public class Chapter1_9Test : BaseTest
     {
@@ -34,10 +37,52 @@ namespace Demo.LearnByDoing.Tests.Chapter01
 
             Assert.Equal(expected, actual);
         }
+
+        [Theory]
+        [ClassData(typeof(Chapter1_9Data))]
+        public void TestIfSubstringIsRotation2(string s1, string s2, bool expected)
+        {
+            bool actual = _sut.IsRotationSubstring2(s1, s2);
+
+            Assert.Equal(expected, actual);
+        }
     }
 
     public class Chapter1_9
     {
+        public bool IsRotationSubstring2(string s1, string s2)
+        {
+            if (string.IsNullOrWhiteSpace(s1) || 
+                string.IsNullOrWhiteSpace(s2) || 
+                s1.Length != s2.Length) return false;
+
+            var s2Concatenated = s2 + s2;
+            for (int i = 0; i < s2Concatenated.Length; i++)
+            {
+                StringBuilder temp1 = new StringBuilder(s1.Length);
+                StringBuilder temp2 = new StringBuilder(s1.Length);
+
+                int offset = i;
+                for (int j = 0; j < s1.Length; j++)
+                {
+                    temp1.Append(s1[j]);
+                    temp2.Append(s2Concatenated[offset]);
+
+                    if (temp1.ToString() == temp2.ToString())
+                    {
+                        if (temp1.Length == s1.Length) return true;
+
+                        offset++;
+                        continue;
+                    }
+
+                    break;
+                }
+            }
+
+            return false;
+        }
+
         public bool IsRotationSubstring(string s1, string s2)
         {
             if (string.IsNullOrWhiteSpace(s1) || string.IsNullOrWhiteSpace(s2)) return false;
@@ -59,6 +104,7 @@ namespace Demo.LearnByDoing.Tests.Chapter01
             new object[] { "eabcd", "abcde", true },
             // different length
             new object[] { "aaabbb", "aaabb", false },
+            new object[] { "xxxcyyy", "xxcyyy", false },
             // completely different: not even a rotation
             new object[] { "12345", "54322", false },
             new object[] { "a", "b", false },
