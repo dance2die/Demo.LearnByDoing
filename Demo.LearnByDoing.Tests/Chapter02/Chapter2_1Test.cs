60ac6143e07d681d708e41ab74a9ca34ae580f1b
dance2die <dontbothermewithspam@gmail.com>
2016-12-18 10:44:43 -0500

    Question 2.1: Refactor

--- Demo.LearnByDoing.Tests/Chapter02/Chapter2_1Test.cs
+++ Demo.LearnByDoing.Tests/Chapter02/Chapter2_1Test.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Demo.LearnByDoing.Tests.Core;
 using Xunit;
@@ -38,86 +37,6 @@ namespace Demo.LearnByDoing.Tests.Chapter02
             LinkedList<int> actual = _sut.RemoveDuplicatesWithoutTemporaryBuffer(input);
 
             Assert.True(expected.SequenceEqual(actual));
-        }
-    }
-
-    public class Chapter2_1
-    {
-        /// <summary>
-        /// Second attempt without using a temporary buffer.
-        /// </summary>
-        /// <remarks>Looked at two hints at this point.</remarks>
-        public LinkedList<int> RemoveDuplicatesWithoutTemporaryBuffer(LinkedList<int> input)
-        {
-            // Pointer 1
-            for (int i = 0; i < input.Count; i++)
-            {
-                // Poiner 2
-                for (int j = i + 1; j < input.Count; j++)
-                {
-                    var currentValue = input.ElementAt(i);
-                    var firstNode = input.Find(currentValue);
-                    var lastNode = input.FindLast(currentValue);
-
-                    if (firstNode != lastNode)
-                        input.Remove(lastNode);
-                }
-            }
-
-            return input;
-        }
-
-        /// <summary>
-        /// First try: Remove duplicates using a temporary buffer.
-        /// </summary>
-        public LinkedList<int> RemoveDuplicates(LinkedList<int> input)
-        {
-            LinkedList<int> result = new LinkedList<int>();
-
-            foreach (var value in input)
-            {
-                if (!result.Contains(value))
-                    result.AddLast(value);
-            }
-
-            return result;;
-        }
-    }
-
-    public class Chapter2_1Data : IEnumerable<object[]>
-    {
-        private readonly List<object[]> _data = new List<object[]>
-        {
-            new object[]
-            {
-                new LinkedListWithInit<int> { 3, 1, 1, 2, 2, 3 },
-                new LinkedListWithInit<int> { 3, 1, 2 },
-            },
-            new object[]
-            {
-                new LinkedListWithInit<int> { 4, 5, 5, 1, 2, 3, 1 },
-                new LinkedListWithInit<int> { 4, 5, 1, 2, 3 },
-            },
-            new object[]
-            {
-                new LinkedListWithInit<int> { 1, 1, 2, 2, 3, 3 },
-                new LinkedListWithInit<int> { 1, 2, 3 },
-            },
-            new object[]
-            {
-                new LinkedListWithInit<int> { 1, 2, 3 },
-                new LinkedListWithInit<int> { 1, 2, 3 },
-            },
-        };
-
-        public IEnumerator<object[]> GetEnumerator()
-        {
-            return _data.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
         }
     }
 
