98d5127606c1df1d5f6bce0a877b6ade7059a370
dance2die <dontbothermewithspam@gmail.com>
2016-12-18 19:55:37 -0500

    Question 2.2: Refactored Chapter2_2Data

--- Demo.LearnByDoing.Tests/Chapter02/Chapter2_2Test.cs
+++ Demo.LearnByDoing.Tests/Chapter02/Chapter2_2Test.cs
@@ -21,12 +21,45 @@ namespace Demo.LearnByDoing.Tests.Chapter02
 
         [Theory]
         [ClassData(typeof(Chapter2_2Data))]
-        public void TestGettingKthElementToTheLastElementOfASinglyLinkedList(
+        public void TestGettingKthElementToTheLastElements(
             LinkedList<int> input, int k, LinkedList<int> expected)
         {
             LinkedList<int> actual = _sut.GetKthToLastElements(input, k);
-
+            
             Assert.True(expected.SequenceEqual(actual));
+        }
+
+        [Theory]
+        [ClassData(typeof(Chapter2_2Data))]
+        public void TestGettingKthElementToTheLastElementOfASinglyLinkedList(
+            Node<int> input, int k, Node<int> expected)
+        {
+            //Node<int> actual = _sut.GetKthToLastElementsOfNode(input, k);
+            
+            
+        }
+    }
+
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        private T _data;
+
+        public Node(T data)
+        {
+            _data = data;
+        }
+
+        public void AddLast<T>(T data)
+        {
+            Node<T> currentNode = this as Node<T>;
+            while (currentNode.Next != null)
+            {
+                currentNode = currentNode.Next;
+            }
+
+            Node<T> newNode = new Node<T>(data);
+            currentNode.Next = newNode;
         }
     }
 
@@ -45,34 +78,39 @@ namespace Demo.LearnByDoing.Tests.Chapter02
         }
     }
 
-    public class Chapter2_2Data : IEnumerable<object[]>
+    public class Chapter2_2Data2 : IEnumerable<object[]>
     {
         private readonly List<object[]> _data = new List<object[]>
         {
             new object[]
             {
-                new LinkedListWithInit<int> { 1, 2, 3, 4, 5 }, 0, new LinkedListWithInit<int> { 1, 2, 3, 4, 5 }
-            },
-            new object[]
-            {
-                new LinkedListWithInit<int> { 1, 2, 3, 4, 5 }, 1, new LinkedListWithInit<int> { 2, 3, 4, 5 }
-            },
-            new object[]
-            {
-                new LinkedListWithInit<int> { 1, 2, 3, 4, 5 }, 2, new LinkedListWithInit<int> { 3, 4, 5 }
-            },
-            new object[]
-            {
-                new LinkedListWithInit<int> { 1, 2, 3, 4, 5 }, 3, new LinkedListWithInit<int> { 4, 5 }
-            },
-            new object[]
-            {
-                new LinkedListWithInit<int> { 1, 2, 3, 4, 5 }, 4, new LinkedListWithInit<int> { 5 }
-            },
-            new object[]
-            {
-                new LinkedListWithInit<int> { 1, 2, 3, 4, 5 }, 5, new LinkedListWithInit<int> { }
-            },
+                
+            }
+        };
+
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            return _data.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class Chapter2_2Data : IEnumerable<object[]>
+    {
+        private static readonly LinkedListWithInit<int> _input = new LinkedListWithInit<int> { 1, 2, 3, 4, 5 };
+
+        private readonly List<object[]> _data = new List<object[]>
+        {
+            new object[] { _input, 0, _input },
+            new object[] { _input, 1, new LinkedListWithInit<int> { 2, 3, 4, 5 } },
+            new object[] { _input, 2, new LinkedListWithInit<int> { 3, 4, 5 } },
+            new object[] { _input, 3, new LinkedListWithInit<int> { 4, 5 } },
+            new object[] { _input, 4, new LinkedListWithInit<int> { 5 } },
+            new object[] { _input, 5, new LinkedListWithInit<int> { } }
         };
 
         public IEnumerator<object[]> GetEnumerator()
