fad7e69bfffe6ec3e7c631bbeb029b1c56b67bec
dance2die <dontbothermewithspam@gmail.com>
2016-12-18 20:21:08 -0500

    Question 2.2: Refactor

---
+++ Demo.LearnByDoing.Tests/Chapter02/Node.cs
@@ -0,0 +1,25 @@
+﻿namespace Demo.LearnByDoing.Tests.Chapter02
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+        }
+
+        public void AddLast<T>(T data)
+        {
+            Node<T> currentNode = this as Node<T>;
+            while (currentNode.Next != null)
+            {
+                currentNode = currentNode.Next;
+            }
+
+            Node<T> newNode = new Node<T>(data);
+            currentNode.Next = newNode;
+        }
+    }
+}
