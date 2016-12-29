ab1f6d723c3ea55a5f1fa67f51e408d9b41f2905
dance2die <dontbothermewithspam@gmail.com>
2016-12-29 07:52:49 -0500

    Question 2.7: Created a test class.

---
+++ Demo.LearnByDoing.Tests/Chapter02/Chapter2_7Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Demo.LearnByDoing.Tests.Chapter02
+{
+    /// <summary>
+    /// Intersection:
+    /// Given two (singly) linked lists, determine if the two lists intersect.
+    /// Return the intersecting node. Note that the intersection is defined based on reference, not value.
+    /// That is, if the kth node of the first linked list is the exact same node (by reference)
+    /// as the jth node of the second linked list, then they are intersecting
+    /// </summary>
+    public class Chapter2_7Test : Chapter2TestBase
+    {
+        private readonly Chapter2_7 _sut = new Chapter2_7();
+
+        public Chapter2_7Test(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+
+    public class Chapter2_7
+    {
+    }
+}
