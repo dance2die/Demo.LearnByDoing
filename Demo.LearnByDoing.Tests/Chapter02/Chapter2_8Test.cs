2a21e4a83b4c9d28cc361823ddb61c72e4c9c3a4
dance2die <dontbothermewithspam@gmail.com>
2016-12-31 12:30:11 -0500

    Question 2.8: Created a failing test, "TestDetectingNodeLoopStart"

--- Demo.LearnByDoing.Tests/Chapter02/Chapter2_8Test.cs
+++ Demo.LearnByDoing.Tests/Chapter02/Chapter2_8Test.cs
@@ -1,4 +1,6 @@
-﻿using Xunit.Abstractions;
+﻿using System.Collections.Generic;
+using Xunit;
+using Xunit.Abstractions;
 
 namespace Demo.LearnByDoing.Tests.Chapter02
 {
@@ -22,9 +24,52 @@ namespace Demo.LearnByDoing.Tests.Chapter02
         public Chapter2_8Test(ITestOutputHelper output) : base(output)
         {
         }
+
+        [Theory]
+        [ClassData(typeof(Chapter2_8Data))]
+        public void TestDetectingNodeLoopStart(Node<string> node, string expected)
+        {
+            Assert.False(true);
+        }
     }
 
     public class Chapter2_8
     {
+    }
+
+    public class Chapter2_8Data : Chapter2Data
+    {
+        public override List<object[]> Data { get; set; } = new List<object[]>
+        {
+            new object[] { GetLoopingNode1(), "C" },
+            new object[] { GetLoopingNode2(), "B" }
+        };
+
+        private static Node<string> GetLoopingNode1()
+        {
+            Node<string> result = new Node<string>("A");
+
+            result.Next = new Node<string>("B");
+            Node<string> loopStart = new Node<string>("C");
+            result.Next = loopStart;
+            result.Next = new Node<string>("D");
+            result.Next = new Node<string>("E");
+            result.Next = loopStart;
+
+            return result;
+        }
+
+        private static Node<string> GetLoopingNode2()
+        {
+            Node<string> result = new Node<string>("A");
+
+            Node<string> loopStart = new Node<string>("B");
+            result.Next = loopStart;
+            result.Next = new Node<string>("C");
+            result.Next = new Node<string>("D");
+            result.Next = loopStart;
+
+            return result;
+        }
     }
 }
