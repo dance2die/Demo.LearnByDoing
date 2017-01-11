9e37612320d4d3d8b2d4deebb63888c636370578
dance2die <dontbothermewithspam@gmail.com>
2017-01-10 21:05:39 -0500

    Question 4.2: Created a test class.

---
+++ Demo.LearnByDoing.Tests/Chapter04/Chapter4_2Test.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Demo.LearnByDoing.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.LearnByDoing.Tests.Chapter04
+{
+    /// <summary>
+    /// MINIMAL TREE:
+    /// Given a sorted (increasing order) array with unique integer elements,
+    /// write an alogrithm to create a binary search tree with minimal height.
+    /// </summary>
+    public class Chapter4_2Test : BaseTest
+    {
+        private readonly Chapter4_2 _sut = new Chapter4_2();
+
+        public Chapter4_2Test(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+
+    public class Chapter4_2
+    {
+    }
+
+    public class Chapter4_2Data : Chapter4TestData
+    {
+        public override List<object[]> Data { get; set; } = new List<object[]>
+        {
+            
+        };
+    }
+}
