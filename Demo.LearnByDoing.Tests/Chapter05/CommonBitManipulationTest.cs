10ec30f612c63ca7e621d53c50982a040e93b320
dance2die <dontbothermewithspam@gmail.com>
2017-01-27 07:16:38 -0500

    learned how to get a bit value in an integer.

---
+++ Demo.LearnByDoing.Tests/Chapter05/CommonBitManipulationTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Demo.LearnByDoing.Tests.Core;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Demo.LearnByDoing.Tests.Chapter05
+{
+    public class CommonBitManipulationTest : BaseTest
+    {
+        private readonly CommonBitManipulation _sut = new CommonBitManipulation();
+
+        public CommonBitManipulationTest(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void TestGettingSettingBits()
+        {
+            int val8 = 8;
+            for (int i = 0; i < 4; i++)
+            {
+                _output.WriteLine("{0} gets bit: {1} at position {2}", Convert.ToString(val8, 2), GetBit(val8, i), i);
+            }
+        }
+
+        private bool GetBit(int num, int i)
+        {
+            int mask = 1 << i;
+            return (num & mask) != 0;
+        }
+
+        private int SetBit(int num, int i)
+        {
+            int mask = 1 << 1;
+            return (num | mask);
+        }
+    }
+
+    public class CommonBitManipulation
+    {
+    }
+}
