57f839228efcaf014ef46366244c301239ca4519
dance2die <dontbothermewithspam@gmail.com>
2017-01-28 06:43:42 -0500

    Clearing bit test passes!

--- Demo.LearnByDoing.Tests/Chapter05/CommonBitManipulationTest.cs
+++ Demo.LearnByDoing.Tests/Chapter05/CommonBitManipulationTest.cs
@@ -19,13 +19,13 @@ namespace Demo.LearnByDoing.Tests.Chapter05
             int val8 = 8;
             for (int i = 0; i < 4; i++)
             {
-                _output.WriteLine("{0} gets bit: {1} at position {2}", Convert.ToString(val8, 2), GetBit(val8, i), i);
+                _output.WriteLine("{0} gets bit: {1} at position {2}", Convert.ToString(val8, 2), _sut.GetBit(val8, i), i);
             }
 
-            Assert.False(GetBit(val8, 0));
-            Assert.False(GetBit(val8, 1));
-            Assert.False(GetBit(val8, 2));
-            Assert.True(GetBit(val8, 3));
+            Assert.False(_sut.GetBit(val8, 0));
+            Assert.False(_sut.GetBit(val8, 1));
+            Assert.False(_sut.GetBit(val8, 2));
+            Assert.True(_sut.GetBit(val8, 3));
         }
 
         [Fact]
@@ -33,26 +33,42 @@ namespace Demo.LearnByDoing.Tests.Chapter05
         {
             int value = 0;
 
-            Assert.Equal(1, SetBit(value, 0));
-            Assert.Equal(2, SetBit(value, 1));
-            Assert.Equal(4, SetBit(value, 2));
-            Assert.Equal(8, SetBit(value, 3));
+            Assert.Equal(1, _sut.SetBit(value, 0));
+            Assert.Equal(2, _sut.SetBit(value, 1));
+            Assert.Equal(4, _sut.SetBit(value, 2));
+            Assert.Equal(8, _sut.SetBit(value, 3));
         }
 
-        private bool GetBit(int num, int i)
+        [Fact]
+        public void TestClearingBits()
         {
-            int mask = 1 << i;
-            return (num & mask) != 0;
-        }
+            int value = 239;
 
-        private int SetBit(int num, int i)
-        {
-            int mask = 1 << i;
-            return (num | mask);
+            Assert.Equal(238, _sut.ClearBit(value, 0));
+            Assert.Equal(237, _sut.ClearBit(value, 1));
+            Assert.Equal(235, _sut.ClearBit(value, 2));
+            Assert.Equal(231, _sut.ClearBit(value, 3));
         }
     }
 
     public class CommonBitManipulation
     {
+        public int ClearBit(int num, int i)
+        {
+            var mask = ~(1 << i);
+            return num & mask;
+        }
+
+        public bool GetBit(int num, int i)
+        {
+            int mask = 1 << i;
+            return (num & mask) != 0;
+        }
+
+        public int SetBit(int num, int i)
+        {
+            int mask = 1 << i;
+            return (num | mask);
+        }
     }
 }
