0ea58044cd89a9f8db43f66d9540c91dbc21c75c
dance2die <dontbothermewithspam@gmail.com>
2017-02-04 06:17:42 -0500

    Chapter5_3: created a test

---
+++ Demo.LearnByDoing.Tests/Chapter05/Chapter5_3Test.cs
@@ -0,0 +1,28 @@
+﻿using Demo.LearnByDoing.Tests.Core;
+using Xunit.Abstractions;
+
+namespace Demo.LearnByDoing.Tests.Chapter05
+{
+    /// <summary>
+    /// Flip Bit to Win:
+    /// You have an integer and you can flip exactly one bit from a 0 to a 1.
+    /// Write code to find the length of the longest sequence of 1s you could create.
+    /// 
+    /// EXAMPLE
+    /// Input:  1775 (or: 11011101111)
+    /// Output: 8 => Flipping 7th bit turns 1775 to => 110 + 11111111
+    /// Hints: #159, #226, #314, #352
+    /// </summary>
+    public class Chapter5_3Test : BaseTest
+    {
+        private readonly Chapter5_3 _sut = new Chapter5_3();
+
+        public Chapter5_3Test(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+
+    public class Chapter5_3
+    {
+    }
+}
