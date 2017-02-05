64f304d3fa2d8797427a6c33adeaba2beaf3810d
dance2die <dontbothermewithspam@gmail.com>
2017-02-05 17:55:14 -0500

    Chapter5_3: able to get sequence count correctly.

--- Demo.LearnByDoing.Tests/Chapter05/Chapter5_3Test.cs
+++ Demo.LearnByDoing.Tests/Chapter05/Chapter5_3Test.cs
@@ -1,4 +1,9 @@
-﻿using Demo.LearnByDoing.Tests.Core;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using Demo.LearnByDoing.Tests.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.LearnByDoing.Tests.Chapter05
@@ -20,9 +25,92 @@ namespace Demo.LearnByDoing.Tests.Chapter05
         public Chapter5_3Test(ITestOutputHelper output) : base(output)
         {
         }
+
+        [Theory]
+        [ClassData(typeof(Chapter5_3Data_SequentialCount))]
+        public void TestGettingSequentialCount(int number, List<int> expected)
+        {
+            var actual = _sut.GetOneCount(number).ToList();
+
+            Assert.True(expected.SequenceEqual(actual));
+        }
+
+        [Theory]
+        [ClassData(typeof(Chapter5_3Data))]
+        public void TestGettingLongestSequenceOfOnes(int number, int expected)
+        {
+            int actual = _sut.GetLongestSequenceOfOnes(number);
+
+            Assert.Equal(expected, actual);
+        }
     }
 
     public class Chapter5_3
     {
+        public int GetLongestSequenceOfOnes(int number)
+        {
+            // Get the list containing sequential 1s.
+            // e.g.) 110 1110 1111 => 2, 3, 4
+            List<int> oneCount = GetOneCount(number).ToList();
+
+            // get the sum of next adjacent item
+            // get the max of the sum
+            int max = 0;
+            for (int i = 0; i < oneCount.Count - 1; i++)
+            {
+                int sum = oneCount[i] + oneCount[i + 1];
+                if (sum > max)
+                    max = sum;
+            }
+
+            return max;
+        }
+
+        // 32 bit integer.
+        private const int INT_BIT_SIZE = 32;
+
+        public IEnumerable<int> GetOneCount(int number)
+        {
+            int startValue = 1;
+            int sequenceSize = 0;
+
+            Func<int, string> toBin = value => Convert.ToString(value, 2);
+
+            for (int i = 0; i < INT_BIT_SIZE; i++)
+            {
+                int mask = startValue << i;
+                if ((mask & number) >= 1)
+                {
+                    sequenceSize++;
+                }
+                else
+                {
+                    if (sequenceSize != 0)
+                        yield return sequenceSize;
+
+                    sequenceSize = 0;
+                }
+
+                mask <<= i + 1;
+            }
+        }
+    }
+
+    public class Chapter5_3Data : TestDataBase
+    {
+        public override List<object[]> Data { get; set; } = new List<object[]>
+        {
+            new object[] {1775, 8}, // 11011101111 => 8
+            new object[] {1463, 6}, // 10110110111 => 6
+        };
+    }
+
+    public class Chapter5_3Data_SequentialCount : TestDataBase
+    {
+        public override List<object[]> Data { get; set; } = new List<object[]>
+        {
+            new object[] {1775, new List<int> {4, 3, 2}},
+            new object[] {1463, new List<int> {3, 2, 2, 1}},
+        };
     }
 }
