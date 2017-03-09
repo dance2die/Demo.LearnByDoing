25fbfb5003f30f15dba37720874a6778239a9d77
dance2die <dontbothermewithspam@gmail.com>
2017-03-08 19:41:19 -0500

    "QuickSortProgram" stub.

---
+++ Demo.LearnByDoing.General/Sort/QuickSortProgram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.LearnByDoing.General.Sort
+{
+    /// <summary>
+    /// Using Algorithm on MyCodeSchool Youtube channel
+    /// https://youtu.be/COk73cpQbFQ
+    /// </summary>
+    public class QuickSortProgram
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
