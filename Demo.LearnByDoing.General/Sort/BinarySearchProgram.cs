dbbfd5ef0efa8937199b026789235385f7a2e4ae
dance2die <dontbothermewithspam@gmail.com>
2017-03-09 07:16:00 -0500

    Binary search using recursion implemented.

--- Demo.LearnByDoing.General/Sort/BinarySearchProgram.cs
+++ Demo.LearnByDoing.General/Sort/BinarySearchProgram.cs
@@ -18,6 +18,23 @@ namespace Demo.LearnByDoing.General.Sort
                 int searchIndex = BinarySearch(a, searchValue);
                 Console.WriteLine("value({0}) => index({1})", searchValue, searchIndex);
             }
+
+            foreach (int searchValue in searchValues)
+            {
+                int searchIndex = BinarySearchRecursive(a, searchValue, 0, a.Length - 1);
+                Console.WriteLine("Recursion: value({0}) => index({1})", searchValue, searchIndex);
+            }
+        }
+
+        private static int BinarySearchRecursive(int[] a, int x, int low, int high)
+        {
+            if (low > high) return -1;  // could not find.
+
+            int mid = (low + high) / 2;
+            if (a[mid] < x) return BinarySearchRecursive(a, x, mid + 1, high);
+            if (a[mid] > x) return BinarySearchRecursive(a, x, low, mid - 1);
+
+            return mid;
         }
 
         private static int BinarySearch(int[] a, int x)
