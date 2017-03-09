13b8d1d8a94dd2fbed695fb45ce98629edfc42c4
dance2die <dontbothermewithspam@gmail.com>
2017-03-09 07:35:10 -0500

    chapter10_1: stub.

---
+++ Demo.LearnByDoing.Tests/Chapter10/Chapter10_1Main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.LearnByDoing.Core;
+using Xunit.Abstractions;
+
+namespace Demo.LearnByDoing.Tests.Chapter10
+{
+    public class Chapter10_1Main : BaseTest
+    {
+        private readonly Chapter10_1 _sut = new Chapter10_1();
+
+        public Chapter10_1Main(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+
+    public class Chapter10_1
+    {
+    }
+}
