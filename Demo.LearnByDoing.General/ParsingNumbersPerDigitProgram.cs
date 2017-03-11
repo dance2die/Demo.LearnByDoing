39044026d54bda0eb3d0e65cd3fa4afa42954d2d
dance2die <dontbothermewithspam@gmail.com>
2017-03-11 11:18:02 -0500

    runs one million times to benchmark.

--- Demo.LearnByDoing.General/ParsingNumbersPerDigitProgram.cs
+++ Demo.LearnByDoing.General/ParsingNumbersPerDigitProgram.cs
@@ -1,20 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace Demo.LearnByDoing.General
 {
     public class ParsingNumbersPerDigitProgram
     {
+        private const int UPTO = 1000000;
+
         public static void Main(string[] args)
         {
             //int val = 192837465;
             int val = 123456789;
-            List<int> digits = GetDigits(val);
 
-            digits.ForEach(Console.WriteLine);
+            Stopwatch watch = new Stopwatch();
+
+            watch.Start();
+            for (int i = 0; i < UPTO; i++)
+            {
+                List<int> digits = GetDigits(val);
+            }
+            watch.Stop();
+            Console.WriteLine("GetDigit took {0}ms", watch.ElapsedMilliseconds);
+
+            watch.Start();
+            for (int j = 0; j < UPTO; j++)
+            {
+                List<int> digits2 = GetDigitsUsingConversion(val);
+            }
+            
+            watch.Stop();
+            Console.WriteLine("GetDigitsUsingConversion took {0}ms", watch.ElapsedMilliseconds);
+
+            //digits.ForEach(Console.WriteLine);
 
             Console.ReadKey();
+        }
+
+        private static List<int> GetDigitsUsingConversion(int val)
+        {
+            return val.ToString().ToCharArray().Select(c => (int) c).ToList();
         }
 
         private static List<int> GetDigits(int val)
