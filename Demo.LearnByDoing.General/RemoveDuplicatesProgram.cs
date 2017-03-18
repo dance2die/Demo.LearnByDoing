422f2ab776d565e3897ea9bc22bde48b733637e3
dance2die <dontbothermewithspam@gmail.com>
2017-03-18 09:52:09 -0400

    Implemented "RemoveDuplicatesBySortingFirst"

--- Demo.LearnByDoing.General/RemoveDuplicatesProgram.cs
+++ Demo.LearnByDoing.General/RemoveDuplicatesProgram.cs
@@ -14,16 +14,22 @@ namespace Demo.LearnByDoing.General
             int[] unordered1 = { 8, 4, 4, 3, 1, 3 };
             int[] sorted1 = RemoveDuplicatesUsingHashTable(unordered1);
             PrintArray("Using HashTable", sorted1);
-            //// 2.) Sort then check next duplicate element.
-            //int[] unordered2 = { 8, 4, 4, 3, 1, 3 };
-            //int[] sorted2 = RemoveDuplicatesBySortingFirst(unordered2);
-            //PrintArray("Sort unordered array first", sorted2);
+            // 2.) Sort then check next duplicate element.
+            int[] unordered2 = { 8, 4, 4, 3, 1, 3 };
+            int[] sorted2 = RemoveDuplicatesBySortingFirst(unordered2);
+            PrintArray("Sort unordered array first", sorted2);
 
             // Ordered Array
             // 1.) By checking next element or previous element: https://youtu.be/kdAiCZQVuvI
             int[] ordered = { 1, 2, 2, 2, 3, 4, 4, 5, 5, 5, 5, 5, 6, 6, 7, 7, 7, 7, 8, 9, 10 };
             int[] sorted3 = RemoveDuplicatesByCheckingPreviousElement(ordered);
             PrintArray("Next Element", sorted3);
+        }
+
+        private static int[] RemoveDuplicatesBySortingFirst(int[] a)
+        {
+            Array.Sort(a);
+            return RemoveDuplicatesByCheckingPreviousElement(a);
         }
 
         private static int[] RemoveDuplicatesByCheckingPreviousElement(int[] a)
