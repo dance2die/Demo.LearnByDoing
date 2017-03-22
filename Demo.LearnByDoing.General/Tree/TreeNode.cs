54cc895bb443f658660b62eb115606b98ea06de5
dance2die <dontbothermewithspam@gmail.com>
2017-03-22 06:45:51 -0400

    Implemented "In-Order" tree traversal.

---
+++ Demo.LearnByDoing.General/Tree/TreeNode.cs
@@ -0,0 +1,21 @@
+﻿namespace Demo.LearnByDoing.General.Tree
+{
+    public class TreeNode<T>
+    {
+        public T Value { get; set; }
+        public TreeNode<T> Left { get; set; }
+        public TreeNode<T> Right { get; set; }
+
+        public TreeNode(T value)
+        {
+            Value = value;
+        }
+
+        public TreeNode(T value, TreeNode<T> left, TreeNode<T> right)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+        }
+    }
+}
---
+++ Demo.LearnByDoing.General/Tree/TreeTraversalProgram.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.LearnByDoing.General.Tree
+{
+    public class TreeTraversalProgram
+    {
+        public static void Main(string[] args)
+        {
+            TreeNode<int> root = CreateSampleBinaryTree();
+
+            List<int> inOrderTraversal = new List<int>();
+            InOrderTraversal(root, inOrderTraversal);
+            Console.Write("In-Order Traversal: {0}", string.Join(" ", inOrderTraversal.Select(val => val.ToString()).ToArray()));
+            Console.WriteLine();
+        }
+
+        private static void InOrderTraversal(TreeNode<int> node, List<int> list)
+        {
+            if (node == null) return;
+
+            InOrderTraversal(node.Left, list);
+            list.Add(node.Value);
+            InOrderTraversal(node.Right, list);
+        }
+
+        private static TreeNode<int> CreateSampleBinaryTree()
+        {
+            TreeNode<int> root = new TreeNode<int>(4)
+            {
+                Left = new TreeNode<int>(2, new TreeNode<int>(1), new TreeNode<int>(3)),
+                Right = new TreeNode<int>(6, new TreeNode<int>(5), new TreeNode<int>(7))
+            };
+            return root;
+        }
+    }
+}
