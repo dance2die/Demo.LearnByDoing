efcb1efbdb736941e04e166d76d58d9b948e0d3f
dance2die <dontbothermewithspam@gmail.com>
2017-04-07 07:29:23 -0400

    "BreadthFirstTraversal" stub.

---
+++ Demo.LearnByDoing.General/Tree/BreadthFirstTraversal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.LearnByDoing.General.Tree
+{
+    /// <summary>
+    /// Traverse a tree using Breadth-first search algorithm.
+    /// </summary>
+    public class BreadthFirstTraversal
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
