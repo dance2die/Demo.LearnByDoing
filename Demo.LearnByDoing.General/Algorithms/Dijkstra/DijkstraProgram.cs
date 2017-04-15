3ae10c234792345ae1c74f133fe6fc6f955fb7a5
dance2die <dontbothermewithspam@gmail.com>
2017-04-15 08:13:39 -0400

    "DijkstraProgram" creates a graph based on https://youtu.be/zXfDYaahsNA

---
+++ Demo.LearnByDoing.General/Algorithms/Dijkstra/DijkstraProgram.cs
@@ -0,0 +1,35 @@
+﻿namespace Demo.LearnByDoing.General.Algorithms.Dijkstra
+{
+    public class DijkstraProgram
+    {
+        public static void Main(string[] args)
+        {
+            Graph<char> graph = CreateSampleGraph();
+        }
+
+        /// <summary>
+        /// Create a graph based on https://youtu.be/zXfDYaahsNA
+        /// </summary>
+        /// <returns></returns>
+        private static Graph<char> CreateSampleGraph()
+        {
+            Graph<char> graph = new Graph<char>();
+
+            var a = new Node<char>('A');
+            var b = new Node<char>('B');
+            var c = new Node<char>('C');
+            var d = new Node<char>('D');
+            var e = new Node<char>('E');
+            var f = new Node<char>('F');
+            var g = new Node<char>('G');
+
+            graph.AddVertex(a, new[] { new Edge<char>(5, b), new Edge<char>(10, b) });
+            graph.AddVertex(b, new[] { new Edge<char>(6, d), new Edge<char>(3, e) });
+            graph.AddVertex(d, new[] { new Edge<char>(6, f) });
+            graph.AddVertex(e, new[] { new Edge<char>(2, c), new Edge<char>(2, d), new Edge<char>(2, g) });
+            graph.AddVertex(g, new[] { new Edge<char>(2, f) });
+
+            return graph;
+        }
+    }
+}
---
+++ Demo.LearnByDoing.General/Algorithms/Dijkstra/Graph.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace Demo.LearnByDoing.General.Algorithms.Dijkstra
+{
+    public class Graph<T>
+    {
+        public List<Node<T>> Nodes { get; set; }
+
+        private readonly Dictionary<Node<T>, Edge<T>[]> _distances = new Dictionary<Node<T>, Edge<T>[]>();
+
+        public void AddVertex(Node<T> node, Edge<T>[] edges)
+        {
+            if (!_distances.ContainsKey(node))
+                _distances.Add(node, edges);
+        }
+    }
+
+    public class Node<T>
+    {
+        public T Value { get; set; }
+
+        public Node(T value)
+        {
+            Value = value;
+        }
+    }
+
+    public class Edge<T>
+    {
+        public int Weight { get; set; }
+        public Node<T> Node { get; set; }
+
+        public Edge(int weight, Node<T> node)
+        {
+            Weight = weight;
+            Node = node;
+        }
+    }
+}
