f52d678fe556e3ce00dc4ca3da8c4f291ccb7b7e
dance2die <dontbothermewithspam@gmail.com>
2017-05-24 06:16:49 -0400

    Implemented Lowest Common Ancestor of Binary Tree

--- Demo.LearnByDoing.General/Tree/TreeNode.cs
+++ Demo.LearnByDoing.General/Tree/TreeNode.cs
@@ -17,5 +17,24 @@
             Left = left;
             Right = right;
         }
+
+	    public static bool operator==(TreeNode<T> left, TreeNode<T> right)
+	    {
+		    if (ReferenceEquals(left, right)) return true;
+		    if (ReferenceEquals(left, null)) return false;
+		    if (ReferenceEquals(null, right)) return false;
+
+		    return left.Value.Equals(right.Value);
+	    }
+
+	    public static bool operator !=(TreeNode<T> left, TreeNode<T> right)
+	    {
+		    return !(left == right);
+	    }
+
+	    public override bool Equals(object obj)
+	    {
+		    return Value.Equals(obj as TreeNode<T>);
+	    }
     }
 }
