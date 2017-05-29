c457d83b217c8203e855b6d1213627ffd96e493e
dance2die <dontbothermewithspam@gmail.com>
2017-05-29 07:44:11 -0400

    Sorts a list at random.

--- Demo.LearnByDoing.General/Sort/SortBasedOnAnotherListProgram.cs
+++ Demo.LearnByDoing.General/Sort/SortBasedOnAnotherListProgram.cs
@@ -12,14 +12,20 @@ namespace Demo.LearnByDoing.General.Sort
 			string[] words = {"string1", "string2", "string3", "string4"};
 
 			// sort "words" when "a" is reversed.
-			var reversedWords = SortByReversing(a, words).ToList();
+			var reversedWords = SortAtRandom(a, words).ToList();
 			reversedWords.ForEach(text => Console.Write($"{text} "));
 		}
 
-		private static IEnumerable<string> SortByReversing(int[] a, string[] words)
+		private static IEnumerable<string> SortAtRandom(int[] a, string[] words)
 		{
 			var zipped = a.Zip(words, (key, word) => Tuple.Create(key, word)).ToList();
-			zipped.Sort((x, y) => -1 * x.Item1.CompareTo(y.Item1));
+			//zipped.Sort((x, y) => -1 * x.Item1.CompareTo(y.Item1));
+			zipped.Sort((x, y) => Guid.NewGuid().CompareTo(Guid.NewGuid()));
+			foreach (Tuple<int, string> tuple in zipped)
+			{
+				Console.Write($"{tuple.Item1} ");
+			}
+			Console.WriteLine();
 			return zipped.Select(z => z.Item2);
 		}
 	}
