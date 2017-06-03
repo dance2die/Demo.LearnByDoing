f3cf6fe833f9d8b10400f49f61f497d9f20233ef
dance2die <dontbothermewithspam@gmail.com>
2017-06-03 09:49:46 -0400

    implementation of build prefix table using YouTube algorithm.

--- Demo.LearnByDoing.General/Search/KmpAlgorithmProgram.cs
+++ Demo.LearnByDoing.General/Search/KmpAlgorithmProgram.cs
@@ -10,13 +10,71 @@ namespace Demo.LearnByDoing.General.Search
 		{
 			string word = "abxabcabcabyabcaby";
 			string searchWord = "abcaby";
+			searchWord = "abcdabca";
+			//searchWord = "acacabacacabacacac";
 			//searchWord = "abxc";
 			//searchWord = "abcdabd";
 
 			KmpSearch kmpSearch = new KmpSearch();
+			//KmpSearch2 kmpSearch2 = new KmpSearch2();
 
-			bool isFound = kmpSearch.SearchUsingKmp(word, searchWord);
+			//bool isFound = kmpSearch2.SearchUsingKmp(word, searchWord);
+			bool isFound = SearchUsingKmp(word, searchWord);
 			Console.WriteLine($"{searchWord} is found within {word}? {isFound}");
+		}
+
+		private static bool SearchUsingKmp(string word, string searchWord)
+		{
+			int[] prefixTable = BuildPrefixTable(searchWord);
+			return false;
+		}
+
+		/// <summary>
+		/// Build Prefix table using Algorithm in YouTube video
+		/// https://youtu.be/GTJr8OvyEVQ
+		/// </summary>
+		/// <param name="searchWord"></param>
+		/// <returns></returns>
+		private static int[] BuildPrefixTable(string searchWord)
+		{
+			int j = 0;
+			int i = j + 1;
+			int[] T = Enumerable.Repeat(0, searchWord.Length).ToArray();
+			T[0] = 0;
+
+			while (i < searchWord.Length)
+			{
+				if (searchWord[i] == searchWord[j])
+				{
+					T[i] = j + 1;
+					j++;
+					i++;
+				}
+				else
+				{
+					//T[i] = 0;
+					//i++;
+
+					// Move to the index pointed by the previous item.
+					//j = T[j - 1];
+					//while (j - 1 >= 0 && searchWord[T[j - 1]] != T[i])
+					while (j - 1 >= 0 && searchWord[T[j - 1]] != searchWord[i])
+					{
+						j--;
+						j = T[j];
+
+						if (j - 1 >= 0 && searchWord[i] == searchWord[T[j - 1]])
+						{
+							T[i] = j - 1;
+							j++;
+						}
+					}
+
+					i++;
+				}
+			}
+
+			return T;
 		}
 	}
 }
