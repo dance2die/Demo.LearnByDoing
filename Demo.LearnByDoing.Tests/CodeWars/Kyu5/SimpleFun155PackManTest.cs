132de587b88535f5961da980423a55ebeab53f12
dance2die <dontbothermewithspam@gmail.com>
2017-07-15 11:07:37 -0400

    Kind of cheated for "PyramidSlideDownTest" since I copied the solution for the same problem in ProjectEuler

--- Demo.LearnByDoing.Tests/CodeWars/Kyu5/SimpleFun155PackManTest.cs
+++ Demo.LearnByDoing.Tests/CodeWars/Kyu5/SimpleFun155PackManTest.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Demo.LearnByDoing.Core;
 using Xunit;
 using Xunit.Abstractions;
