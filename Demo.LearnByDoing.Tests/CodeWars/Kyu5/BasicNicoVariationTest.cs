df2a7f1e2ffd7983a32cf88d6fe21cfc4e7aea88
Sung <sungmkim80@gmail.com>
2017-07-23 21:24:17 -0400

    Added test cases for BasicNicoVariationTest

---
+++ Demo.LearnByDoing.Tests/CodeWars/Kyu5/BasicNicoVariationTest.cs
@@ -0,0 +1,33 @@
+﻿using Demo.LearnByDoing.Core;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Demo.LearnByDoing.Tests.CodeWars.Kyu5
+{
+    public class BasicNicoVariationTest : BaseTest
+    {
+        public BasicNicoVariationTest(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Theory]
+        [InlineData("crazy", "secretinformation", "cseerntiofarmit on  ")]
+        [InlineData("abc", "abcd", "abcd  ")]
+        [InlineData("ba", "1234567890", "2143658709")]
+        [InlineData("a", "message", "message")]
+        [InlineData("key", "key", "eky")]
+        public void TestSampleNico(string key, string message, string expected)
+        {
+            string actual = Kata.Nico(key, message);
+            Assert.Equal(expected, actual);
+        }
+    }
+    
+    public partial class Kata 
+    {
+        public static string Nico(string key, string message) 
+        {
+            return message;
+        }
+    }
+}
