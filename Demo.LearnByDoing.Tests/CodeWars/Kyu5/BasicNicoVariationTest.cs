ad68e58445a950592de29abaa138ffddb8f30a07
Sung <sungmkim80@gmail.com>
2017-07-24 20:52:25 -0400

    BasicNicoVariationTest passed!

--- Demo.LearnByDoing.Tests/CodeWars/Kyu5/BasicNicoVariationTest.cs
+++ Demo.LearnByDoing.Tests/CodeWars/Kyu5/BasicNicoVariationTest.cs
@@ -1,9 +1,16 @@
-﻿using Demo.LearnByDoing.Core;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Demo.LearnByDoing.Core;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.LearnByDoing.Tests.CodeWars.Kyu5
 {
+    /// <summary>
+    /// https://www.codewars.com/kata/basic-nico-variation/csharp
+    /// </summary>
     public class BasicNicoVariationTest : BaseTest
     {
         public BasicNicoVariationTest(ITestOutputHelper output) : base(output)
@@ -25,9 +32,52 @@ namespace Demo.LearnByDoing.Tests.CodeWars.Kyu5
     
     public partial class Kata 
     {
-        public static string Nico(string key, string message) 
+        public static string Nico(string key, string message)
         {
-            return message;
+            Dictionary<char, char[]> map = BuildMessageMap(key, message);
+//            var orderedValues = map.OrderBy(pair => pair.Key).SelectMany(pair => pair.Value).ToArray();
+
+            StringBuilder buffer = new StringBuilder();
+            for (int i = 0; i < map.Values.FirstOrDefault().Length; i++)
+            {
+                foreach (var obj in map.OrderBy(pair => pair.Key).Select((pair, index) => new {pair.Value, index}))
+                {
+                    buffer.Append(obj.Value[i]);
+                }
+            }
+
+            return buffer.ToString();
+        }
+
+        private static Dictionary<char, char[]> BuildMessageMap(string key, string message)
+        {
+            Dictionary<char, char[]> result = new Dictionary<char, char[]>(key.Length);
+            int rowCount = (int) Math.Ceiling((double)message.Length / key.Length);
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < key.Length; j++)
+                {
+                    var k = key[j];
+                
+                    if (!result.ContainsKey(k))
+                        result.Add(k, new char[rowCount]);
+
+                    var messageIndex = j + (i * key.Length);
+                    result[k][i] = messageIndex > message.Length - 1 ? ' ' : message[messageIndex];
+                }
+            }
+           
+            return result;
+        }
+
+        private static Dictionary<int, List<string>> BuildMap(List<int> keyOrder, string message)
+        {
+            var map = new Dictionary<int, List<string>>();
+            int arrayLength = (int) Math.Ceiling((double) (message.Length / keyOrder.Count));
+
+            
+            return map;
         }
     }
 }
