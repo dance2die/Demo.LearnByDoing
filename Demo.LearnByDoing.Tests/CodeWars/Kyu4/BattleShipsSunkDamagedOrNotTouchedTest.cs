997963d1f076998fc56e13c159554e0e63da91f1
Sung Kim <dance2die@hotmail.com>
2017-08-04 15:34:49 -0400

    failing tests.

--- Demo.LearnByDoing.Tests/CodeWars/Kyu4/BattleShipsSunkDamagedOrNotTouchedTest.cs
+++ Demo.LearnByDoing.Tests/CodeWars/Kyu4/BattleShipsSunkDamagedOrNotTouchedTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Demo.LearnByDoing.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.LearnByDoing.Tests.CodeWars.Kyu4
@@ -15,6 +16,57 @@ namespace Demo.LearnByDoing.Tests.CodeWars.Kyu4
 	{
 		public BattleShipsSunkDamagedOrNotTouchedTest(ITestOutputHelper output) : base(output)
 		{
+		}
+
+		[Fact]
+		public void BasicTest1()
+		{
+			var kata = new Kata();
+			int[,] board = { { 0, 0, 1, 0 },
+				{ 0, 0, 1, 0 },
+				{ 0, 0, 1, 0 } };
+			int[,] attacks = { { 3, 1 }, { 3, 2 }, { 3, 3 } };
+			var result = Kata.damagedOrSunk(board, attacks);
+			Console.WriteLine("Test 1 - sunk = 1, damaged = 0, notTouched = 0, points = 1");
+			Assert.Equal(1, result["sunk"]);
+			Assert.Equal(0, result["damaged"]);
+			Assert.Equal(0, result["notTouched"]);
+			Assert.Equal(1, result["points"]);
+		}
+
+		[Fact]
+		public void BasicTest2()
+		{
+			var kata = new Kata();
+			int[,] board = { { 3, 0, 1 },
+				{ 3, 0, 1 },
+				{ 0, 2, 1 },
+				{ 0, 2, 0 } };
+			int[,] attacks = { { 2, 1 }, { 2, 2 }, { 3, 2 }, { 3, 3 } };
+			var result = Kata.damagedOrSunk(board, attacks);
+			Console.WriteLine("Test 2 - sunk = 1, damaged = 1, notTouched = 1, points = 0.5");
+			Assert.Equal(1, result["sunk"]);
+			Assert.Equal(1, result["damaged"]);
+			Assert.Equal(1, result["notTouched"]);
+			Assert.Equal(0.5, result["points"]);
+		}
+	}
+
+	partial class Kata
+	{
+		public static Dictionary<string, double> damagedOrSunk(int[,] board, int[,] attacks)
+		{
+			Dictionary<string, double> result = new Dictionary<string, double>
+			{
+				{ "sunk",  0 },
+				{ "damaged", 0 },
+				{ "notTouched",  0 },
+				{ "points", 0 },
+			};
+
+
+
+			return result;
 		}
 	}
 }
