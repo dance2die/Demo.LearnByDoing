68f4fc653a308941fea2cfbcc3b69832287e397e
Sung Kim <dance2die@hotmail.com>
2017-08-04 14:58:53 -0400

    created "BattleShipsSunkDamagedOrNotTouchedTest"

---
+++ Demo.LearnByDoing.Tests/CodeWars/Kyu4/BattleShipsSunkDamagedOrNotTouchedTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.LearnByDoing.Core;
+using Xunit.Abstractions;
+
+namespace Demo.LearnByDoing.Tests.CodeWars.Kyu4
+{
+	/// <summary>
+	/// https://www.codewars.com/kata/58d06bfbc43d20767e000074/train/csharp
+	/// </summary>
+	public class BattleShipsSunkDamagedOrNotTouchedTest : BaseTest
+	{
+		public BattleShipsSunkDamagedOrNotTouchedTest(ITestOutputHelper output) : base(output)
+		{
+		}
+	}
+}
