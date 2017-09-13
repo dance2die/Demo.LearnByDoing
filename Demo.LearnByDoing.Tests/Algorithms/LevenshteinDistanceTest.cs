22e2487e2cd0c7f50ec8ceb04ddc8d09d6cf3fcd
dance2die <dontbothermewithspam@gmail.com>
2017-09-13 07:05:08 -0400

    Removed unused references

--- Demo.LearnByDoing.Tests/Algorithms/LevenshteinDistanceTest.cs
+++ Demo.LearnByDoing.Tests/Algorithms/LevenshteinDistanceTest.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Demo.LearnByDoing.Core;
 using Xunit;
 using Xunit.Abstractions;
