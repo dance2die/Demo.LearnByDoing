0a93a44afc49f4ad6b41f2760bfc28e4669dff24
dance2die <dontbothermewithspam@gmail.com>
2017-09-29 06:58:28 -0400

    Implemented "Stack.push" using SungLinkedList

--- Demo.LearnByDoing.Tests/MissionInterview/01-DataStructures/StackTest.cs
+++ Demo.LearnByDoing.Tests/MissionInterview/01-DataStructures/StackTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Demo.LearnByDoing.Core;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.LearnByDoing.Tests.MissionInterview
@@ -15,6 +16,43 @@ namespace Demo.LearnByDoing.Tests.MissionInterview
 	{
 		public StackTest(ITestOutputHelper output) : base(output)
 		{
+		}
+
+		[Fact]
+		public void TestPush()
+		{
+			var sut = new SungStack<int>();
+			sut.Push(1);
+			sut.Push(2);
+			sut.Push(3);
+
+			int[] expected = {3, 2, 1};
+			Assert.True(expected.SequenceEqual(sut.Traverse().ToArray()));
+		}
+	}
+
+	public class SungStack<T>
+	{
+		private readonly SungLinkedList<T> _list;
+		private int _count;
+
+		public SungStack()
+		{
+			_list = new SungLinkedList<T>();
+			_count = 0;
+		}
+
+		public void Push(T value)
+		{
+			_count++;
+			var newNode = new SungNode<T>(value);
+			newNode.Next = _list.Head;
+			_list.Head = newNode;
+		}
+
+		public IEnumerable<T> Traverse()
+		{
+			return _list.Traverse().Select(o => o.Value);
 		}
 	}
 }
