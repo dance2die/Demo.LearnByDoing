e247abac4bdbec593c913a7683dc6b4454349a65
dance2die <dontbothermewithspam@gmail.com>
2017-09-30 11:03:40 -0400

    Moved SungQueue to its own file.

--- Demo.LearnByDoing.Tests/MissionInterview/01-DataStructures/QueueTest.cs
+++ Demo.LearnByDoing.Tests/MissionInterview/01-DataStructures/QueueTest.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using Demo.LearnByDoing.Core;
 using Xunit;
@@ -54,39 +53,6 @@ namespace Demo.LearnByDoing.Tests.MissionInterview._01_DataStructures
 			}
 
 			Assert.Throws<InvalidOperationException>(() => sut.Dequeue());
-		}
-	}
-
-	public class SungQueue<T>
-	{
-		private int _count = 0;
-		private readonly DoublySungLinkedList<T> _list = new DoublySungLinkedList<T>();
-
-		public void Enqueue(T value)
-		{
-			_count++;
-			_list.Append(value);
-		}
-
-		public IEnumerable<T> Traverse()
-		{
-			return _list.Traverse().Select(a => a.Value);
-		}
-
-		public T Dequeue()
-		{
-			if (_count == 0)
-				throw new InvalidOperationException("Queue empty.");
-
-			_count--;
-			var result = _list.Head;
-			_list.Remove(_list.Head);
-			return result.Value;
-		}
-
-		public T Peek()
-		{
-			return _list.Head == null ? default(T) : _list.Head.Value;
 		}
 	}
 }
