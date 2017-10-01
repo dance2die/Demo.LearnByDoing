a2f14d350900c233c3efcb721e83c2f530db116f
dance2die <dontbothermewithspam@gmail.com>
2017-10-01 09:08:59 -0400

    Created a failing test.

---
+++ Demo.LearnByDoing.Tests/CodeWars/Kyu6/WhoLikesItTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Demo.LearnByDoing.Tests.CodeWars.Kyu6
+{
+	/// <summary>
+	/// https://www.codewars.com/kata/who-likes-it/train/csharp
+	/// </summary>
+	public class WhoLikesItTest
+	{
+		[Theory]
+		[InlineData("no one likes this", new string[0])]
+		[InlineData("Peter likes this", new string[] { "Peter" })]
+		[InlineData("Jacob and Alex like this", new string[] { "Jacob", "Alex" })]
+		[InlineData("Max, John and Mark like this", new string[] { "Max", "John", "Mark" })]
+		[InlineData("Alex, Jacob and 2 others like this", new string[] { "Alex", "Jacob", "Mark", "Max" })]
+		public void SampleTestCases(string expected, string[] input)
+		{
+			var actual = Kata.Likes(input);
+			Assert.Equal(expected, actual);
+		}
+	}
+
+	public static partial class Kata
+	{
+		public static string Likes(string[] name)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
