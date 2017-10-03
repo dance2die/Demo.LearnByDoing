eebb0dc4e66d669d045349020df665132fbae6f3
dance2die <dontbothermewithspam@gmail.com>
2017-10-03 06:29:37 -0400

    Implemented "SungBinaryTreeNode", which will be extended to create SungBinarySearchTreeNode

---
+++ Demo.LearnByDoing.Tests/MissionInterview/01-DataStructures/SungTreeTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.LearnByDoing.Core;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Demo.LearnByDoing.Tests.MissionInterview._01_DataStructures
+{
+	public class SungTreeTest : BaseTest
+	{
+		public SungTreeTest(ITestOutputHelper output) : base(output)
+		{
+		}
+
+		
+	}
+
+	public class SungBinaryTreeNode<T>
+	{
+		public T Value { get; set; }
+		public SungBinaryTreeNode<T> Left { get; set; }
+		public SungBinaryTreeNode<T> Right { get; set; }
+
+		public SungBinaryTreeNode(T value)
+		{
+			Value = value;
+		}
+
+		public SungBinaryTreeNode<T> AddLeft(T value)
+		{
+			Left = new SungBinaryTreeNode<T>(value);
+			return this;
+		}
+
+		public SungBinaryTreeNode<T> AddRight(T value)
+		{
+			Right = new SungBinaryTreeNode<T>(value);
+			return this;
+		}
+	}
+}
