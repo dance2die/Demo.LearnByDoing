71829ceca4abdc6f031ff97573079870342770b9
dance2die <dontbothermewithspam@gmail.com>
2017-10-03 07:15:41 -0400

    Created "SungVertex"

---
+++ Demo.LearnByDoing.Tests/MissionInterview/01-DataStructures/SungGraphTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Demo.LearnByDoing.Tests.MissionInterview._01_DataStructures
+{
+	public class SungGraphTest
+	{
+	}
+
+	public class SungVertex<T>
+	{
+		public T Value { get; set; }
+		public Dictionary<SungVertex<T>, int> Edges { get; }
+
+		public SungVertex(T value)
+		{
+			Value = value;
+			Edges = new Dictionary<SungVertex<T>, int>();
+		}
+
+		public SungVertex<T> To(SungVertex<T> toVertex, int cost)
+		{
+			if (!Edges.ContainsKey(toVertex))
+				Edges.Add(toVertex, cost);
+			return this;
+		}
+	}
+}
