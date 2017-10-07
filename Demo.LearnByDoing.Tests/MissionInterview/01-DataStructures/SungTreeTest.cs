b1a1c4ef12ef0a7df56ae3b3dc2b7f9309294036
dance2die <dontbothermewithspam@gmail.com>
2017-10-07 07:53:59 -0400

    Submitted "ConvertALinkedListToAStringTest" on CodeWars.com successfully on first try!

--- Demo.LearnByDoing.Tests/MissionInterview/01-DataStructures/SungTreeTest.cs
+++ Demo.LearnByDoing.Tests/MissionInterview/01-DataStructures/SungTreeTest.cs
@@ -15,7 +15,38 @@ namespace Demo.LearnByDoing.Tests.MissionInterview._01_DataStructures
 		{
 		}
 
-		
+		public SungBinaryTreeNode<int> SampleTree()
+		{
+			return new SungBinaryTreeNode<int>(1000)
+			{
+				Left = new SungBinaryTreeNode<int>(500)
+				{
+					Left = new SungBinaryTreeNode<int>(400)
+					{
+						Left = new SungBinaryTreeNode<int>(220),
+						Right = new SungBinaryTreeNode<int>(260)
+					},
+					Right = new SungBinaryTreeNode<int>(450)
+					{
+						Left = new SungBinaryTreeNode<int>(250),
+						Right = new SungBinaryTreeNode<int>(280)
+					}
+				},
+				Right = new SungBinaryTreeNode<int>(500)
+				{
+					Left = new SungBinaryTreeNode<int>(380)
+					{
+						Left = new SungBinaryTreeNode<int>(210),
+						Right = new SungBinaryTreeNode<int>(220)
+					},
+					Right = new SungBinaryTreeNode<int>(350)
+					{
+						Left = new SungBinaryTreeNode<int>(230),
+						Right = new SungBinaryTreeNode<int>(240)
+					}
+				}
+			};
+		}
 	}
 
 	public class SungBinaryTreeNode<T>
