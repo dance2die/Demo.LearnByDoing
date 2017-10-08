891957720789bc53b692c48203b0f4cedd15e3c1
dance2die <dontbothermewithspam@gmail.com>
2017-10-08 08:26:47 -0400

    "TestKeyGeneration" passes

--- Demo.LearnByDoing.Tests/CodeWars/Kyu6/CondiCipherTest.cs
+++ Demo.LearnByDoing.Tests/CodeWars/Kyu6/CondiCipherTest.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Demo.LearnByDoing.Core;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Demo.LearnByDoing.Tests.CodeWars.Kyu6
 {
-	public class CondiCipherTest
+	public class CondiCipherTest : BaseTest
 	{
+		public CondiCipherTest(ITestOutputHelper output) : base(output)
+		{
+		}
+
 		[Theory]
 		[InlineData("jx", "on", "cryptogam", 10)]
 		[InlineData("cytgmdfmbk", "cryptogram", "cryptogam", 0)]
@@ -32,10 +38,22 @@ namespace Demo.LearnByDoing.Tests.CodeWars.Kyu6
 			Assert.Equal(expected, Kata.Decode(key, m, initShift));
 		}
 
-		[Theory]
+		[Fact]
 		public void TestKeyGeneration()
 		{
-			
+			// cryptogambdefhijklnqsuvwxz
+			Dictionary<char, int> expected = new Dictionary<char, int>
+			{
+				{'c', 1}, {'r', 2}, {'y', 3}, {'p', 4}, {'t', 5},
+				{'o', 6}, {'g', 7}, {'a', 8}, {'m', 9}, {'b', 10},
+				{'d', 11}, {'e', 12}, {'f', 13}, {'h', 14}, {'i', 15},
+				{'j', 16}, {'k', 17}, {'l', 18}, {'n', 19}, {'q', 20}, 
+				{'s', 21}, {'u', 22}, {'v', 23}, {'w', 24}, {'x', 25}, 
+				{'z', 26}
+			};
+
+			var actual = Kata.GetMap("cryptogram");
+			Assert.True(expected.SequenceEqual(actual));
 		}
 	}
 
@@ -48,6 +66,24 @@ namespace Demo.LearnByDoing.Tests.CodeWars.Kyu6
 		public static string Decode(string key, string m, int initShift)
 		{
 			return m;
+		}
+
+		public static Dictionary<char, int> GetMap(string key)
+		{
+			var map = key
+				.Distinct()
+				.Select((c, i) => new { Key = c, Value = i + 1 })
+				.ToDictionary(o => o.Key, o => o.Value);
+
+			int offset = map.Count;
+			var aToZ = Enumerable.Range('a', 'z' - 'a' + 1).ToList();
+			var rest = aToZ.Where(n => !map.ContainsKey((char) n)).ToList();
+			for (int i = 0; i < aToZ.Count - offset; i++)
+			{
+				map.Add((char)rest[i], i + offset + 1);
+			}
+
+			return map;
 		}
 	}
 }
