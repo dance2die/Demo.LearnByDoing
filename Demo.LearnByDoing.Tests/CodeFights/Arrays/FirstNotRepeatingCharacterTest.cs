3b25497d9e2471505d94f969e3bb87e8e9f7c113
dance2die <dontbothermewithspam@gmail.com>
2017-10-13 07:24:29 -0400

    Added failing test for "FirstNotRepeatingCharacterTest"

---
+++ Demo.LearnByDoing.Tests/CodeFights/Arrays/FirstNotRepeatingCharacterTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Demo.LearnByDoing.Tests.CodeFights.Arrays
+{
+	/// <summary>
+	/// https://codefights.com/interview-practice/task/uX5iLwhc6L5ckSyNC
+	/// </summary>
+	public class FirstNotRepeatingCharacterTest
+	{
+		[Theory]
+		[InlineData('c', "abacabad")]
+		[InlineData('_', "abacabaabacaba")]
+		[InlineData('z', "z")]
+		[InlineData('c', "bcb")]
+		[InlineData('_', "bcccccccb")]
+		[InlineData('d', "abcdefghijklmnopqrstuvwxyziflskecznslkjfabe")]
+		[InlineData('_', "zzz")]
+		[InlineData('y', "bcccccccccccccyb")]
+		[InlineData('d', "xdnxxlvupzuwgigeqjggosgljuhliybkjpibyatofcjbfxwtalc")]
+		[InlineData('g', "ngrhhqbhnsipkcoqjyviikvxbxyphsnjpdxkhtadltsuxbfbrkof")]
+		public void SampleTests(char expected, string input)
+		{
+			char actual = firstNotRepeatingCharacter(input);
+			Assert.Equal(expected, actual);
+		}
+
+		char firstNotRepeatingCharacter(string s)
+		{
+			return ' ';
+		}
+	}
+}
