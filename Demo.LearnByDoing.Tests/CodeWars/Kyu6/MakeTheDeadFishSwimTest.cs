f56c1e0419b67b712a5b4c8f789380d9260074c7
dance2die <dontbothermewithspam@gmail.com>
2017-10-24 21:41:38 -0400

    "MakeTheDeadFishSwimTest" submitted on CodeWars.com successfully!

---
+++ Demo.LearnByDoing.Tests/CodeWars/Kyu6/MakeTheDeadFishSwimTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Demo.LearnByDoing.Tests.CodeWars.Kyu6
+{
+	/// <summary>
+	/// https://www.codewars.com/kata/make-the-deadfish-swim/train/csharp
+	/// </summary>
+	[TestFixture]
+	public class MakeTheDeadFishSwimTest
+	{
+		private static object[] _sampleTestCases = {
+			new object[] {"iiisdoso", new[] {8, 64}},
+			new object[] {"iiisdosodddddiso", new[] {8, 64, 3600}}
+		};
+
+		[Test, TestCaseSource(nameof(_sampleTestCases))]
+		public void SampleTest(string data, int[] expected)
+		{
+			Assert.AreEqual(expected, Deadfish.Parse(data));
+		}
+	}
+
+	public class Deadfish
+	{
+		public static int[] Parse(string data)
+		{
+			var result = new List<int>();
+
+			var commands = new Dictionary<char, Func<int, int>>
+			{
+				{'i', n => ++n },
+				{'d', n => --n },
+				{'s', n => n * n },
+				{'o', n => { result.Add(n); return n; } }
+			};
+
+			var value = 0;
+			data.Select(c => value = commands[c](value)).ToList();
+
+			return result.ToArray();
+		}
+	}
+}
