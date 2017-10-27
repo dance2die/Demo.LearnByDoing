54d4d5823034e05e925b66ef5bcf5b35ea507ad7
dance2die <dontbothermewithspam@gmail.com>
2017-10-27 06:17:55 -0400

    Submitted "ConsecutiveStringsTest" on CodeWars.com successfully.

--- Demo.LearnByDoing.Tests/CodeWars/Kyu6/ConsecutiveStringsTest.cs
+++ Demo.LearnByDoing.Tests/CodeWars/Kyu6/ConsecutiveStringsTest.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace Demo.LearnByDoing.Tests.CodeWars.Kyu6
@@ -13,33 +10,53 @@ namespace Demo.LearnByDoing.Tests.CodeWars.Kyu6
 	[TestFixture]
 	public class ConsecutiveStringsTest
 	{
-		private static void testing(string actual, string expected)
+		private static void Testing(string actual, string expected)
 		{
 			Assert.AreEqual(expected, actual);
 		}
 
 		[Test]
-		public static void test1()
+		public static void Test1()
 		{
 			Console.WriteLine("Basic Tests");
-			testing(LongestConsecutives.LongestConsec(new String[] { "zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail" }, 2), "abigailtheta");
-			testing(LongestConsecutives.LongestConsec(new String[] { "ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh" }, 1), "oocccffuucccjjjkkkjyyyeehh");
-			testing(LongestConsecutives.LongestConsec(new String[] { }, 3), "");
-			testing(LongestConsecutives.LongestConsec(new String[] { "itvayloxrp", "wkppqsztdkmvcuwvereiupccauycnjutlv", "vweqilsfytihvrzlaodfixoyxvyuyvgpck" }, 2), "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck");
-			testing(LongestConsecutives.LongestConsec(new String[] { "wlwsasphmxx", "owiaxujylentrklctozmymu", "wpgozvxxiu" }, 2), "wlwsasphmxxowiaxujylentrklctozmymu");
-			testing(LongestConsecutives.LongestConsec(new String[] { "zone", "abigail", "theta", "form", "libe", "zas" }, -2), "");
-			testing(LongestConsecutives.LongestConsec(new String[] { "it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz" }, 3), "ixoyx3452zzzzzzzzzzzz");
-			testing(LongestConsecutives.LongestConsec(new String[] { "it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz" }, 15), "");
-			testing(LongestConsecutives.LongestConsec(new String[] { "it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz" }, 0), "");
+			Testing(LongestConsecutives.LongestConsec(new String[] { "zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail" }, 2), "abigailtheta");
+			Testing(LongestConsecutives.LongestConsec(new String[] { "ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh" }, 1), "oocccffuucccjjjkkkjyyyeehh");
+			Testing(LongestConsecutives.LongestConsec(new String[] { }, 3), "");
+			Testing(LongestConsecutives.LongestConsec(new String[] { "itvayloxrp", "wkppqsztdkmvcuwvereiupccauycnjutlv", "vweqilsfytihvrzlaodfixoyxvyuyvgpck" }, 2), "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck");
+			Testing(LongestConsecutives.LongestConsec(new String[] { "wlwsasphmxx", "owiaxujylentrklctozmymu", "wpgozvxxiu" }, 2), "wlwsasphmxxowiaxujylentrklctozmymu");
+			Testing(LongestConsecutives.LongestConsec(new String[] { "zone", "abigail", "theta", "form", "libe", "zas" }, -2), "");
+			Testing(LongestConsecutives.LongestConsec(new String[] { "it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz" }, 3), "ixoyx3452zzzzzzzzzzzz");
+			Testing(LongestConsecutives.LongestConsec(new String[] { "it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz" }, 15), "");
+			Testing(LongestConsecutives.LongestConsec(new String[] { "it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz" }, 0), "");
 		}
 	}
 
 	public class LongestConsecutives
 	{
-
-		public static string LongestConsec(string[] strarr, int k)
+		public static string LongestConsec(string[] a, int k)
 		{
-			return "";
+			var n = a.Length;
+			if (n == 0 || k > n || k <= 0) return string.Empty;
+
+			var max = 0;
+			var maxStartIndex = 0;
+
+			for (int i = 0; i <= n - k; i++)
+			{
+				var curr = 0;
+				for (int j = i; j < k + i; j++)
+				{
+					curr += a[j].Length;
+				}
+
+				if (curr > max)
+				{
+					max = curr;
+					maxStartIndex = i;
+				}
+			}
+
+			return string.Concat(a.Skip(maxStartIndex).Take(k));
 		}
 	}
 }
