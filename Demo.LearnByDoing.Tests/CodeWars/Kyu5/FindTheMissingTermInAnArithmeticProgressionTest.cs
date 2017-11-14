f66e2b2a164a64b703fa576c992d1c22beaede30
Sung Kim <dance2die@hotmail.com>
2017-11-14 16:48:58 -0500

    Added more test cases...

--- Demo.LearnByDoing.Tests/CodeWars/Kyu5/FindTheMissingTermInAnArithmeticProgressionTest.cs
+++ Demo.LearnByDoing.Tests/CodeWars/Kyu5/FindTheMissingTermInAnArithmeticProgressionTest.cs
@@ -16,6 +16,7 @@ namespace Demo.LearnByDoing.Tests.CodeWars.Kyu5
 			{
 				yield return new TestCaseData(new[] { new List<int> { 1, 5, 7, 9, 11 } }).Returns(3);
 				yield return new TestCaseData(new[] { new List<int> { 1, 3, 5, 9, 11 } }).Returns(7);
+				yield return new TestCaseData(new[] { new List<int> { -3, -1, 1, 5, 7 } }).Returns(3);
 				yield return new TestCaseData(new[] { new List<int> { 0, 5, 10, 20, 25 } }).Returns(15);
 				yield return new TestCaseData(new[] { new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11 } }).Returns(10);
 				yield return new TestCaseData(new[] { new List<int> { 1040, 1220, 1580 } }).Returns(1400);
