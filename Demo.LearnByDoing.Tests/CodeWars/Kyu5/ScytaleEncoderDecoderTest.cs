790fd7e749ec8c48504867569dc9b857a371abfa
Sung Kim <dance2die@hotmail.com>
2017-11-15 10:56:41 -0500

    Created a failing test for "ScytaleEncoderDecoderTest"

---
+++ Demo.LearnByDoing.Tests/CodeWars/Kyu5/ScytaleEncoderDecoderTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Demo.LearnByDoing.Tests.CodeWars.Kyu5
+{
+	/// <summary>
+	/// https://www.codewars.com/kata/scytale-encoder-slash-decoder-ancient-spartans-cipher/train/csharp
+	/// </summary>
+	public class ScytaleEncoderDecoderTest
+	{
+		[Test]
+		public void BasicEncodeTest()
+		{
+			var message = "HELPMEIAMUNDERATTACK";
+			var expected = "HENTEIDTLAEAPMRCMUAK";
+			var actual = Scytale.Encode(message, 4);
+
+			Assert.That(actual, Is.EqualTo(expected));
+		}
+
+		[Test]
+		public void BasicDecodeTest()
+		{
+			var message = "HENTEIDTLAEAPMRCMUAK";
+			var expected = "HELPMEIAMUNDERATTACK";
+			var actual = Scytale.Decode(message, 4);
+
+			Assert.That(actual, Is.EqualTo(expected));
+		}
+	}
+
+	public class Scytale
+	{
+		public static string Decode(string message, int cylinderSides)
+		{
+			// TODO: implement me
+			return message;
+		}
+
+		public static string Encode(string message, int cylinderSides)
+		{
+			// TODO: implement me
+
+			return message;
+		}
+	}
+}
