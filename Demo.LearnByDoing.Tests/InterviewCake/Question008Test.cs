13bbc18049bd2f0d408b431772527a11ecafa2da
dance2die <dontbothermewithspam@gmail.com>
2017-11-21 06:59:32 -0500

    Created InterviewCake question #8 Question008Test

---
+++ Demo.LearnByDoing.Tests/InterviewCake/Question008Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.LearnByDoing.Tests.InterviewCake
+{
+	/// <summary>
+	/// Write a function to see if a binary tree ↴ is "superbalanced" (a new tree property we just made up).
+	/// https://www.interviewcake.com/question/csharp/balanced-binary-tree
+	/// </summary>
+	public class Question008Test
+	{
+	}
+}
