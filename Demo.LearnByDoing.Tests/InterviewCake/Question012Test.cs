69842e0d15c0b2668c475b8b1a09b7caa7d6f42b
dance2die <dontbothermewithspam@gmail.com>
2017-11-30 06:13:22 -0500

    All test cases for Question012Test passes.

--- Demo.LearnByDoing.Tests/InterviewCake/Question012Test.cs
+++ Demo.LearnByDoing.Tests/InterviewCake/Question012Test.cs
@@ -15,17 +15,27 @@ namespace Demo.LearnByDoing.Tests.InterviewCake
 
 		public static IEnumerable<object[]> GoodPathData()
 		{
-			yield return new object[] {true, new []{1,2,3,4,5}, 1};
-			yield return new object[] {true, new []{1,2,3,4,5}, 2};
-			yield return new object[] {true, new []{1,2,3,4,5}, 3};
-			yield return new object[] {true, new []{1,2,3,4,5}, 4};
-			yield return new object[] {true, new []{1,2,3,4,5}, 5};
-			yield return new object[] {false, new []{1,2,3,4,5}, 6};
-			yield return new object[] {true, new []{-11,-2,3,4,5}, -11};
-			yield return new object[] {true, new []{-11,-2,3,4,5}, -2};
-			yield return new object[] {true, new []{-11,-2,3,4,5}, -5};
-			yield return new object[] {false, new []{-11,-2,3,4,5}, -99};
-			yield return new object[] {false, new []{-11,-2,3,4,5}, 6};
+			yield return new object[] { true, new[] { 1, 2, 3, 4, 5 }, 1 };
+			yield return new object[] { true, new[] { 1, 2, 3, 4, 5 }, 2 };
+			yield return new object[] { true, new[] { 1, 2, 3, 4, 5 }, 3 };
+			yield return new object[] { true, new[] { 1, 2, 3, 4, 5 }, 4 };
+			yield return new object[] { true, new[] { 1, 2, 3, 4, 5 }, 5 };
+			yield return new object[] { false, new[] { 1, 2, 3, 4, 5 }, 6 };
+			yield return new object[] { true, new[] { -11, -2, 3, 4, 5 }, -11 };
+			yield return new object[] { true, new[] { -11, -2, 3, 4, 5 }, -2 };
+			yield return new object[] { false, new[] { -11, -2, 3, 4, 5 }, -5 };
+			yield return new object[] { false, new[] { -11, -2, 3, 4, 5 }, 6 };
+			yield return new object[] { false, new[] { -11, -2, 3, 4, 5 }, -99 };
+
+			yield return new object[] { true, new[] { 1,2,3,4,5,6,7,8,9 }, 1 };
+			yield return new object[] { true, new[] { 1,2,3,4,5,6,7,8,9 }, 2 };
+			yield return new object[] { true, new[] { 1,2,3,4,5,6,7,8,9 }, 3 };
+			yield return new object[] { true, new[] { 1,2,3,4,5,6,7,8,9 }, 4 };
+			yield return new object[] { true, new[] { 1,2,3,4,5,6,7,8,9 }, 5 };
+			yield return new object[] { true, new[] { 1,2,3,4,5,6,7,8,9 }, 6 };
+			yield return new object[] { true, new[] { 1,2,3,4,5,6,7,8,9 }, 7 };
+			yield return new object[] { true, new[] { 1,2,3,4,5,6,7,8,9 }, 8 };
+			yield return new object[] { true, new[] { 1,2,3,4,5,6,7,8,9 }, 9 };
 		}
 
 		[Theory]
@@ -50,6 +60,29 @@ namespace Demo.LearnByDoing.Tests.InterviewCake
 		{
 			if (a == null) throw new ArgumentNullException();
 
+			// Use Binary Search
+
+			// Get the middle index
+			// Get the value in the middle of the array
+			// If n == middle value then return.
+			// If the value is less than the middle value, then search the left
+			// If the value is greater than the middle value, then search the right
+
+			int startIndex = 0;
+			int endIndex = a.Length - 1;
+
+			while (startIndex <= endIndex)
+			{
+				var middleIndex = (startIndex + endIndex) / 2;
+				var middle = a[middleIndex];
+				if (middle == n) return true;
+
+				if (n < middle)
+					endIndex = middleIndex - 1;
+				else
+					startIndex = middleIndex + 1;
+			}
+			
 			return false;
 		}
 	}
