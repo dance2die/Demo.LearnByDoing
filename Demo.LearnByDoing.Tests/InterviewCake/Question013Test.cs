53003e222f036299c3ad08eaeb288301c298fc0e
dance2die <dontbothermewithspam@gmail.com>
2017-12-03 06:43:16 -0500

    "Question013Test" edge case passes

---
+++ Demo.LearnByDoing.Tests/InterviewCake/Question013Test.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Demo.LearnByDoing.Tests.InterviewCake
+{
+	public class Question013Test
+	{
+		[Fact]
+		public void TestEdgeCases()
+		{
+			const int expected = 0;
+			var sut = new Question013();
+
+			int actual = sut.GetRotationIndex(null);
+			Assert.Equal(expected, actual);
+
+			actual = sut.GetRotationIndex(new string[]{});
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void TestSample()
+		{
+			var words = new string[]
+			{
+				"ptolemaic",
+				"retrograde",
+				"supplant",
+				"undulate",
+				"xenoepist",
+				"asymptote",  // <-- rotates here!
+				"babka",
+				"banoffee",
+				"engender",
+				"karpatka",
+				"othellolagkage",
+			};
+
+			const int expected = 5;
+			int actual = new Question013().GetRotationIndex(words);
+			Assert.Equal(expected, actual);
+		}
+	}
+
+	public class Question013
+	{
+		public int GetRotationIndex(string[] words)
+		{
+			if (words == null || words.Length == 0) return 0;
+
+			return 0;
+		}
+	}
+}
