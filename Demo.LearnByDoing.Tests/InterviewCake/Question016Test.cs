c76b1ba47461e6785a9f7807452e3f2192007cb1
dance2die <dontbothermewithspam@gmail.com>
2017-12-18 11:57:10 -0500

    Tests all pass for the first time for Question016Test

--- Demo.LearnByDoing.Tests/InterviewCake/Question016Test.cs
+++ Demo.LearnByDoing.Tests/InterviewCake/Question016Test.cs
@@ -23,16 +23,62 @@ namespace Demo.LearnByDoing.Tests.InterviewCake
 		{
 			yield return new object[]{555, new []
 			{
+				new CakeType(2, 15),
 				new CakeType(7, 160),
 				new CakeType(3, 90),
-				new CakeType(2, 15),
 			}, 20};
+
+			yield return new object[]{630, new []
+			{
+				new CakeType(2, 15),
+				new CakeType(7, 160),
+				new CakeType(3, 90),
+			}, 21};
 		}
 	}
 
 	internal class Question016
 	{
 		public double MaxDuffelBagValue(CakeType[] cakeTypes, int capacity)
+		{
+			var duffleBag = new int[cakeTypes.Length, capacity + 1];
+
+			for (int rowIndex = 0; rowIndex < cakeTypes.Length; rowIndex++)
+			{
+				int maxCapacity = 0;
+				for (int currentCapacity = 1; currentCapacity <= capacity; currentCapacity++)
+				{
+					var cake = cakeTypes[rowIndex];
+
+					// If we have less capacity than the current cake's weight, then we can't add it to the dufflebag.
+					if (cake.Weight > currentCapacity && rowIndex == 0)
+					{
+						duffleBag[rowIndex, currentCapacity] = 0;
+						continue;
+					}
+
+					var previousMax = rowIndex == 0 ? 0 : duffleBag[rowIndex - 1, currentCapacity];
+					// -- vs. --
+
+					// Value of current item + 
+					var multiplier = currentCapacity / cake.Weight;
+					var currentValue = cake.Value * multiplier;
+
+					// Value of remaining space
+					var remainingSpace = multiplier == 0 ? 0 : currentCapacity - (cake.Weight * multiplier);
+					var remainingSpaceValue = duffleBag[rowIndex, remainingSpace];
+					var currentMax = currentValue + remainingSpaceValue;
+
+					var newMax = (int)Math.Max(previousMax, currentMax);
+					duffleBag[rowIndex, currentCapacity] = newMax;
+				}
+			}
+
+			// Return the last item in the last row.
+			return duffleBag[cakeTypes.Length - 1, capacity];
+		}
+
+		public double MaxDuffelBagValueBad(CakeType[] cakeTypes, int capacity)
 		{
 			var duffleBag = new int[cakeTypes.Length, capacity + 1];
 
