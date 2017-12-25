1036bb2fb1748d84c90d6cdec06d3084d9312462
dance2die <dontbothermewithspam@gmail.com>
2017-12-25 09:18:16 -0500

    Passed all tests on the first try with O(n) runtime complexity and O(1) space complexity

---
+++ Demo.LearnByDoing.Tests/InterviewCake/Question021Test.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Demo.LearnByDoing.Tests.InterviewCake
+{
+	/// <summary>
+	/// https://www.interviewcake.com/question/csharp/find-unique-int-among-duplicates
+	/// 
+	/// Your company delivers breakfast via autonomous quadcopter drones. And something mysterious has happened. 
+	/// 
+	///   Each breakfast delivery is assigned a unique ID, a positive integer. When one of the company's 100 drones takes off with a delivery, the delivery's ID is added to an array, deliveryIdConfirmations. When the drone comes back and lands, the ID is again added to the same array.
+	/// 
+	/// After breakfast this morning there were only 99 drones on the tarmac. One of the drones never made it back from a delivery. We suspect a secret agent from Amazon placed an order and stole one of our patented drones. To track them down, we need to find their delivery ID.
+	/// 
+	/// Given the array of IDs, which contains many duplicate integers and one unique integer, find the unique integer.
+	/// 
+	/// The IDs are not guaranteed to be sorted or sequential. Orders aren't always fulfilled in the order they were received, and some deliveries get cancelled before takeoff. 
+	/// </summary>
+	public class Question021Test
+	{
+		[Theory]
+		[MemberData(nameof(GetSampleData))]
+		public void TestSampleCases(int expected, int[] deliveryIdConfirmations)
+		{
+			int actual = FindUniqueInteger(deliveryIdConfirmations);
+			Assert.Equal(expected, actual);
+		}
+
+		public static IEnumerable<object[]> GetSampleData()
+		{
+			yield return new object[] { 1, new[] { 1 } };
+			yield return new object[] { 1, new[] { 22, 22, 3, 3, 1, 4, 4 } };
+			yield return new object[] { 99, new[] { 99, 22, 22, 3, 3, 1, 1, 4, 4 } };
+			yield return new object[] { 44, new[] { 22, 22, 3, 3, 1, 1, 4, 4, 44 } };
+		}
+
+		private int FindUniqueInteger(int[] deliveryIdConfirmations)
+		{
+			if (deliveryIdConfirmations == null || deliveryIdConfirmations.Length == 0)
+				throw new ArgumentException("There should be at least 1 record", nameof(deliveryIdConfirmations));
+
+			int uniqueValue = deliveryIdConfirmations[0];
+			for (int i = 1; i < deliveryIdConfirmations.Length; i++)
+			{
+				uniqueValue ^= deliveryIdConfirmations[i];
+			}
+
+			return uniqueValue;
+		}
+	}
+}
