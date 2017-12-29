d62a90799df702108296877f839ad5251804957b
dance2die <dontbothermewithspam@gmail.com>
2017-12-29 06:35:08 -0500

    Fixed question numbers again...

--- Demo.LearnByDoing.Tests/InterviewCake/Question025Test.cs
+++ Demo.LearnByDoing.Tests/InterviewCake/Question025Test.cs
@@ -1,24 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Demo.LearnByDoing.Tests.InterviewCake
 {
 	/// <summary>
-	/// https://www.interviewcake.com/question/csharp/reverse-linked-list
+	/// https://www.interviewcake.com/question/csharp/kth-to-last-node-in-singly-linked-list
 	/// 
-	///   Hooray! It's opposite day. Linked lists go the opposite way today.
+	///   You have a linked list ↴ and want to find the kkkth to last node.
 	/// 
-	/// Write a method for reversing a linked list. ↴ Do it in-place. ↴
+	/// Write a method KthToLastNode() that takes an integer kkk and the headNode of a singly-linked list, and returns the kkkth to last node in the list.
 	/// 
-	/// Your method will have one input: the head of the list.
-	/// 
-	/// Your method should return the new head of the list.
-	/// 
-	/// Here's a sample linked list node class:
+	/// For example:
 	/// 
 	/// public class LinkedListNode
 	/// {
@@ -27,57 +19,74 @@ namespace Demo.LearnByDoing.Tests.InterviewCake
 	/// 
 	///		public LinkedListNode(int value)
 	///		{
-	///			Value = value;
+	///			this.Value = value;
 	///		}
 	/// }
 	/// 
+	/// var a = new LinkedListNode(1);
+	/// var b = new LinkedListNode(2);
+	/// var c = new LinkedListNode(3);
+	/// var d = new LinkedListNode(4);
+	/// var e = new LinkedListNode(5);
+	/// 
+	/// a.Next = b;
+	/// b.Next = c;
+	/// c.Next = d;
+	/// d.Next = e;
+	/// 
+	/// // Returns the node with value 4 (the 2nd to last node)
+	/// var node = KthToLastNode(2, a);
 	/// </summary>
 	public class Question025Test
 	{
 		[Fact]
-		public void TestReverse()
+		public void TestSampleCase()
 		{
-			var root = new LinkedListNode(1) {Next = new LinkedListNode(2) {Next = new LinkedListNode(3)}};
-			var expected = new LinkedListNode(3) {Next = new LinkedListNode(2) {Next = new LinkedListNode(1)}};
+			var a = new LinkedListNode(1);
+			var b = new LinkedListNode(2);
+			var c = new LinkedListNode(3);
+			var d = new LinkedListNode(4);
+			var e = new LinkedListNode(5);
 
-			var actual = ReverseLinkedList(root);
-			Assert.True(expected.Traverse().SequenceEqual(actual.Traverse()));
+			a.Next = b;
+			b.Next = c;
+			c.Next = d;
+			d.Next = e;
+
+			// Returns the node with value 4 (the 2nd to last node)
+			var actual = KthToLastNode(2, a).Value;
+			const int expected = 4;
+			Assert.Equal(expected, actual);
 		}
 
-		[Fact]
-		public void TestEdgeCases()
+		public LinkedListNode KthToLastNode(int kth, LinkedListNode head)
 		{
-			Assert.Null(ReverseLinkedList(null));
-			var expected = 1;
-			Assert.Equal(expected, ReverseLinkedList(new LinkedListNode(1)).Value);
-		}
+			var ahead = GetAheadNode(kth, head);
+			if (ahead == null)
+				throw new ArgumentOutOfRangeException();
 
-		private LinkedListNode ReverseLinkedList(LinkedListNode head)
-		{
-			if (head == null) return null;
-
-			LinkedListNode prev = null;
-			var current = head;
-			var next = head.Next;
-
-			//while (true)
-			//{
-			//	current.Next = prev;
-			//	prev = current;
-			//	current = next;
-			//	if (next == null) return prev;
-			//	next = next.Next;
-			//}
-			while (true)
+			var root = head;
+			while (ahead != null)
 			{
-				current.Next = prev;
-				prev = current;
-				if (next == null) return current;
-				current = next;
-				//if (current == null) return prev;
-				next = next.Next;
+				root = root.Next;
+				ahead = ahead.Next;
 			}
+
+			return root;
 		}
+
+		private LinkedListNode GetAheadNode(int kth, LinkedListNode head)
+		{
+			int currentPosition = 0;
+			var root = head;
+			while (root != null && currentPosition < kth)
+			{
+				root = root.Next;
+				currentPosition++;
+			}
+			return root;
+		}
+
 
 		public class LinkedListNode
 		{
@@ -86,17 +95,7 @@ namespace Demo.LearnByDoing.Tests.InterviewCake
 
 			public LinkedListNode(int value)
 			{
-				Value = value;
-			}
-
-			public IEnumerable<int> Traverse()
-			{
-				var root = this;
-				while (root != null)
-				{
-					yield return root.Value;
-					root = root.Next;
-				}
+				this.Value = value;
 			}
 		}
 	}
