4748a2b61f32809c6541ee0f13a95119e7847902
dance2die <dontbothermewithspam@gmail.com>
2018-01-13 05:16:53 -0500

    First passing implementation of Question031

--- Demo.LearnByDoing.Tests/InterviewCake/Question031Test.cs
+++ Demo.LearnByDoing.Tests/InterviewCake/Question031Test.cs
@@ -3,10 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Demo.LearnByDoing.Tests.InterviewCake
 {
-	class Question031Test
+	/// <summary>
+	/// https://www.interviewcake.com/question/csharp/recursive-string-permutations
+	/// 
+	/// 
+	/// 
+	/// Write a recursive method for generating all permutations of an input string. Return them as a set.
+	/// 
+	/// Don't worry about time or space complexity—if we wanted efficiency we'd write an iterative version.
+	/// 
+	/// To start, assume every character in the input string is unique.
+	/// 
+	/// Your method can have loops—it just needs to also be recursive.
+	/// </summary>
+	public class Question031Test
 	{
+		public static IEnumerable<object[]> GetSampleCases()
+		{
+			yield return new object[] { "a", new[] { "a" } };
+			yield return new object[] { "ab", new[] { "ab", "ba" } };
+			yield return new object[] { "abc", new[] { "abc", "acb", "bac", "bca", "cab", "cba" } };
+		}
+
+		[Theory]
+		[MemberData(nameof(GetSampleCases))]
+		public void TestSampleCases(string input, string[] expected)
+		{
+			var sut = new Question031();
+			string[] actual = sut.GetPermutations(input);
+
+			Assert.True(expected.OrderBy(t => t).SequenceEqual(actual.OrderBy(t => t)));
+		}
+	}
+
+	public class Question031
+	{
+		public string[] GetPermutations(string input)
+		{
+			return FillPermutations(input, 0).ToArray();
+		}
+
+		private IEnumerable<string> FillPermutations(string input, int startIndex)
+		{
+			if (startIndex >= input.Length) yield return input;
+			else if (input.Length == 2)
+			{
+				yield return input;
+				yield return new string(new[] { input[1], input[0] });
+			}
+			else
+			{
+				for (int i = 0; i < input.Length; i++)
+				{
+					string prefix = input[i].ToString();
+					int endIndex = i - startIndex;
+					string left = input.Substring(startIndex, endIndex);
+					string right = input.Substring(endIndex + 1);
+					string newInput = left + right;
+
+					foreach (var rest in FillPermutations(newInput, startIndex + 1))
+					{
+						yield return prefix + rest;
+					}
+				}
+			}
+		}
 	}
 }
