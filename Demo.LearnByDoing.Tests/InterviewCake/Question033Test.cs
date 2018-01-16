cc6c7df01cd19a65be59478f24a6e4ea39803286
dance2die <dontbothermewithspam@gmail.com>
2018-01-16 06:12:57 -0500

    Created Question033Test

---
+++ Demo.LearnByDoing.Tests/InterviewCake/Question033Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.LearnByDoing.Tests.InterviewCake
+{
+	/// <summary>
+	/// https://www.interviewcake.com/question/csharp/which-appears-twice
+	/// 
+	/// 
+	/// I have an array of n+1n + 1n+1 numbers. Every number in the range 1..n1..n1..n appears once except for one number that appears twice.
+	/// 
+	/// Write a method for finding the number that appears twice.
+	/// 
+	/// </summary>
+	public class Question033Test
+	{
+	}
+}
