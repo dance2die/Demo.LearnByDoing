472c62e28d80b47ed8e8491cfa0cc72ea69b696c
dance2die <dontbothermewithspam@gmail.com>
2018-01-17 05:39:54 -0500

    Hashversion for Question033Test passes.

---
+++ Demo.LearnByDoing.Tests/CodeFights/CommonTechniques/SumInRangeTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Demo.LearnByDoing.Tests.CodeFights.CommonTechniques
+{
+	/// <summary>
+	/// https://codefights.com/interview-practice/task/4MoqQLaw22nrzXbgs/description
+	/// </summary>
+	public class SumInRangeTest
+	{
+		[Fact]
+		public void TestSumInRange()
+		{
+			int[] nums = { 3, 0, -2, 6, -3, 2 };
+			int[][] queries = new int[3][];
+			queries[0] = new[] { 0, 2 };
+			queries[1] = new[] { 2, 5 };
+			queries[2] = new[] { 0, 5 };
+
+			int actual = GetSumInRange(nums, queries);
+			const int expected = 10;
+
+			Assert.Equal(expected, actual);
+		}
+
+		private int GetSumInRange(int[] nums, int[][] queries)
+		{
+			var forwardSum = new List<int>(nums.Length);
+			//forwardSum.Add(nums[0]);
+			forwardSum[0] = 0;
+			var backwardSum = new List<int>(nums.Length);
+			//backwardSum.Add(nums[nums.Length - 1]);
+			backwardSum[nums.Length - 1] = 0;
+
+			for (int i = 1, j = nums.Length - 2; i < nums.Length; i++, j--)
+			{
+				var forwardCurrent = nums[i];
+				var backwardCurrent = nums[j];
+				var prevForwardNum = forwardSum[i - 1];
+				var prevBackwardNum = backwardSum[j + 1];
+
+				forwardSum[i] = forwardCurrent + prevForwardNum;
+				backwardSum[j] = backwardCurrent + prevBackwardNum;
+
+				//forwardSum.Add(prevForwardNum + forwardCurrent);
+				//backwardSum.Add(prevBackwardNum + backwardCurrent);
+			}
+
+			var range = new List<int>();
+			foreach (var query in queries)
+			{
+				var f = query[0];
+				var t = query[1];
+
+				if (t - f == 1) range.Add(nums[t] + nums[f]);
+				else if (t - f == 0) range.Add(nums[t]);
+				else if (f == 0) range.Add(forwardSum[t]);
+				else if (t == nums.Length - 1) range.Add(backwardSum[f]);
+				else range.Add(backwardSum[t] - forwardSum[f]);
+			}
+
+
+			foreach (var n in range) Console.WriteLine(n);
+
+			var total = range.Sum();
+			var mod = 1000000007;
+			return (total % mod + mod) % mod;
+		}
+	}
+}
--- Demo.LearnByDoing.Tests/InterviewCake/Question033Test.cs
+++ Demo.LearnByDoing.Tests/InterviewCake/Question033Test.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Xunit;
 
 namespace Demo.LearnByDoing.Tests.InterviewCake
 {
@@ -10,12 +8,36 @@ namespace Demo.LearnByDoing.Tests.InterviewCake
 	/// https://www.interviewcake.com/question/csharp/which-appears-twice
 	/// 
 	/// 
-	/// I have an array of n+1n + 1n+1 numbers. Every number in the range 1..n1..n1..n appears once except for one number that appears twice.
+	/// I have an array of n + 1 numbers. Every number in the range 1..n appears once except for one number that appears twice.
 	/// 
 	/// Write a method for finding the number that appears twice.
 	/// 
 	/// </summary>
 	public class Question033Test
 	{
+		[Theory]
+		[MemberData(nameof(GetSampleCases))]
+		public void TestHashVersion(int expected, int[] input)
+		{
+			int actual = FindDupeUsingHash(input);
+			Assert.Equal(expected, actual);
+		}
+
+		public static IEnumerable<object[]> GetSampleCases()
+		{
+			yield return new object[] {3, new[] { 1, 2, 3, 4, 3, 5, 6, 7 } };
+		}
+
+		private int FindDupeUsingHash(int[] input)
+		{
+			var isSeen = new HashSet<int>();
+			foreach (int number in input)
+			{
+				if (isSeen.Contains(number)) return number;
+				isSeen.Add(number);
+			}
+
+			throw new ArgumentException();
+		}
 	}
 }
