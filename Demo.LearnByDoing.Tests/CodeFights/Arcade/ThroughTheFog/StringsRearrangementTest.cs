948de0e5fd37ea8b5717d5951cfe1700405d8646
Sung Kim <sungmkim80@gmail.com>
2018-04-19 06:59:35 -0400

    GetPermutation flattens permutation list...

--- Demo.LearnByDoing.Tests/CodeFights/Arcade/ThroughTheFog/StringsRearrangementTest.cs
+++ Demo.LearnByDoing.Tests/CodeFights/Arcade/ThroughTheFog/StringsRearrangementTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Demo.LearnByDoing.Core;
 using Xunit;
@@ -11,6 +12,10 @@ namespace Demo.LearnByDoing.Tests.CodeFights.Arcade.ThroughTheFog
     /// </summary>
     public class StringsRearrangementTest : BaseTest
     {
+        public StringsRearrangementTest(ITestOutputHelper output) : base(output)
+        {
+        }
+
         [Theory]
         [MemberData(nameof(GetSampleCases))]
         public void TestSamples(bool expected, string[] a)
@@ -33,30 +38,39 @@ namespace Demo.LearnByDoing.Tests.CodeFights.Arcade.ThroughTheFog
 
         bool stringsRearrangement(string[] a)
         {
-            var result = new List<string>(a.Length) { a[0] };
-            var input = new List<string>(a.Skip(1));
 
-            for (int i = 1; i < input.Count; i++)
+            var perms = GetPermutations(a, new List<string>()).ToList();
+            foreach (var s in a)
             {
-                var curr = a[i];
-
-
-                _output.WriteLine($"curr={curr}, DifferentByOne = {string.Join(",", result.Where(t => IsDifferentByOne(curr, t)))}");
-                //if (result.Any(t => IsDifferentByOne(curr, t)))
-                if (result.Any(t => input.Any(ti => IsDifferentByOne(t, ti))))
-                {
-                    result.Add(curr);
-                    input.Remove(curr);
-                    i--;
-                }
-                else
-                {
-                    _output.WriteLine($"no different curr={curr}, DifferentByOne = {string.Join(",", result.Where(t => IsDifferentByOne(curr, t)))}");
-                }
+                if (perms.Any(perm => IsDifferentByOne(s, perm))) return true;
             }
-            _output.WriteLine($"result={string.Join(",", result)}");
 
-            return result.Distinct().Count() == a.Length;
+            return false;
+
+            //var result = new List<string>(a.Length) { a[0] };
+            //var input = new List<string>(a.Skip(1));
+
+            //for (int i = 1; i < input.Count; i++)
+            //{
+            //    var curr = a[i];
+
+
+            //    _output.WriteLine($"curr={curr}, DifferentByOne = {string.Join(",", result.Where(t => IsDifferentByOne(curr, t)))}");
+            //    //if (result.Any(t => IsDifferentByOne(curr, t)))
+            //    if (result.Any(t => input.Any(ti => IsDifferentByOne(t, ti))))
+            //    {
+            //        result.Add(curr);
+            //        input.Remove(curr);
+            //        i--;
+            //    }
+            //    else
+            //    {
+            //        _output.WriteLine($"no different curr={curr}, DifferentByOne = {string.Join(",", result.Where(t => IsDifferentByOne(curr, t)))}");
+            //    }
+            //}
+            //_output.WriteLine($"result={string.Join(",", result)}");
+
+            //return result.Distinct().Count() == a.Length;
         }
 
         bool IsDifferentByOne(string s1, string s2)
@@ -74,9 +88,81 @@ namespace Demo.LearnByDoing.Tests.CodeFights.Arcade.ThroughTheFog
             return true;
         }
 
-
-        public StringsRearrangementTest(ITestOutputHelper output) : base(output)
+        private static IEnumerable<string> GetPermutations(string[] words)
         {
+            return GetPermutations(words, new List<string>());
         }
+
+        private static IEnumerable<string> GetPermutations(IEnumerable<string> words, IEnumerable<string> prefix)
+        {
+            if (words.Count() == 0)
+            {
+                foreach (var p in prefix)
+                    yield return p;
+            }
+
+            var wordList = words.ToList();
+            for (int i = 0; i < wordList.Count; i++)
+            {
+                //string remainder = str.Substring(0, i) + str.Substring(i + 1);
+                var remainder = wordList.Take(i).Concat(wordList.Skip(i + 1));
+                //var nextPrefix = prefix + str[i];
+                var nextPrefix = prefix.Concat(new[] {wordList[i]});
+
+                foreach (var permutation in GetPermutations(remainder, nextPrefix).ToList())
+                    yield return permutation;
+            }
+        }
+
+        //private static IEnumerable<string> Permutation(IEnumerable<string> words, IEnumerable<string> prefix)
+        //{
+        //    if (words.Count() == 0)
+        //    {
+        //        //Console.WriteLine(prefix);
+        //        return prefix;
+        //    }
+
+        //    var result = new List<string>();
+        //    var wordList = words.ToList();
+        //    for (int i = 0; i < wordList.Count; i++)
+        //    {
+        //        string[] remainder = wordList.Take(i).Concat(wordList.Skip(i)).ToArray();
+        //        result.AddRange(Permutation(remainder,  prefix.Concat(new []{ wordList[i] })));
+        //    }
+
+        //    return result.ToArray();
+        //}
+
+        //private IEnumerable<string> GetPermutations(string[] a)
+        //{
+        //    return FillPermutations(a).SelectMany(_ => _);
+        //}
+
+        //private IEnumerable<string[]> FillPermutations(string[] a)
+        //{
+        //    if (a.Length == 1) yield return a[0];
+        //    else if (a.Length == 2)
+        //    {
+        //        yield return a[0];
+        //        yield return a[1];
+        //        yield return a[0];
+        //        //yield return new string(new[] { a[1], a[0] });
+        //    }
+        //    else
+        //    {
+        //        for (int i = 0; i < a.Length; i++)
+        //        {
+        //            string prefix = a[i].ToString();
+        //            string left = a.Take(0, i);
+        //            string right = a.Substring(i + 1);
+        //            string newInput = left + right;
+
+        //            foreach (var rest in FillPermutations(newInput))
+        //            {
+        //                yield return prefix + rest;
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
