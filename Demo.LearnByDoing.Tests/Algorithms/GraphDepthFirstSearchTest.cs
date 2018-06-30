618fc7f13c62784357f1f33a7c6ce38bf2c4cb85
Sung Kim <sungmkim80@gmail.com>
2018-06-29 20:03:01 -0400

    Depth first search of a graph represented with adjacency list format.

--- Demo.LearnByDoing.Tests/Algorithms/GraphDepthFirstSearchTest.cs
+++ Demo.LearnByDoing.Tests/Algorithms/GraphDepthFirstSearchTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
@@ -19,9 +18,48 @@ namespace Demo.LearnByDoing.Tests.Algorithms
             Assert.True(expected.SequenceEqual(actual));
         }
 
+        /// <summary>
+        /// DFS of a graph represented as an adjacency list.
+        /// </summary>
+        /// <summary>
+        /// Create a stack and put the first item in it.
+        /// initialize visited nodes - to get around cycles
+        /// 
+        /// initialize result sequence.
+        /// 
+        /// while the stack is not empty,
+        ///     pop the node
+        /// if visited, then skip
+        /// else add the node to the result
+        ///         and add adjacent vertices to the stack.
+        /// 
+        /// return the result.
+        /// </summary>
+        /// <param name="graph"></param>
+        /// <returns></returns>
         private static int[] DfsAdjacencyList(Dictionary<int, int[]> graph)
         {
-            throw new NotImplementedException();
+            var stack = new Stack<int>();
+            stack.Push(graph.First().Key);
+
+            var visited = new HashSet<int>();
+
+            while (stack.Count > 0)
+            {
+                var vertex = stack.Pop();
+                if (visited.Contains(vertex)) continue;
+
+                visited.Add(vertex);
+                if (!graph.ContainsKey(vertex)) continue;
+                
+                foreach (var neighbor in graph[vertex])
+                {
+                    if (visited.Contains(neighbor)) continue;
+                    stack.Push(neighbor);
+                }
+            }
+
+            return visited.ToArray();
         }
 
         public static IEnumerable<object[]> GetAdjacencyLists()
@@ -31,13 +69,12 @@ namespace Demo.LearnByDoing.Tests.Algorithms
                 new[] {1, 2, 3, 4, 7, 8, 6, 5, 9},
                 new Dictionary<int, int[]>
                 {
-                    {1, new[] {2, 4, 7}},
+                    {1, new[] {7, 4, 2}},
                     {2, new[] {3}},
                     {3, new[] {4}},
-                    {1, new[] {1}},
-                    {7, new[] {8, 6}},
-                    {6, new[] {5, 9}},
-                    {6, new[] {9}},
+                    {4, new[] {1}},
+                    {7, new[] {6, 8}},
+                    {6, new[] {9, 5}},
                     {9, new[] {6}},
                 }
             };
