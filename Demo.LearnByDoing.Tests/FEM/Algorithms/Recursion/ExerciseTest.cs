442711c349bff63fd71809c5c2582b78ac267aa4
Sung Kim <sungmkim80@gmail.com>
2018-07-19 21:32:04 -0400

    Created facts for Recursion

---
+++ Demo.LearnByDoing.Tests/FEM/Algorithms/Recursion/ExerciseTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Demo.LearnByDoing.Tests.FEM.Algorithms.Recursion
+{
+    /// <summary>
+    /// https://github.com/kuychaco/algoClass/blob/master/recursion/recursionIntro.js
+    /// </summary>
+    public class ExerciseTest
+    {
+        /// <summary>
+        /// 1. Write a function that loops through the numbers n down to 0.
+        /// If you haven't done so try using a while loop to do this.
+        /// </summary>
+        [Fact]
+        public void Exercise1()
+        {
+
+        }
+
+        /// <summary>
+        /// 2. Next, try looping just like above except using recursion
+        /// </summary>
+        [Fact]
+        public void Exercise2()
+        {
+
+        }
+
+        /// <summary>
+        /// 3.Write a function 'exponent' that takes two arguments base, and expo,
+        /// uses a while loop to return the exponenet value of the base.
+        /// </summary>
+        [Fact]
+        public void Exercise3()
+        {
+
+        }
+
+        /// <summary>
+        /// 4. Write a function 'RecursiveExponent' that takes two arguments 
+        /// base, and expo, recursively returns exponent value of the base.
+        /// </summary>
+        [Fact]
+        public void Exercise4()
+        {
+
+        }
+
+        /// <summary>
+        /// 5. Write a function 'recursiveMultiplier' that takes two arguments, 
+        /// 'arr and num', and multiplies each arr value into by num and returns an array of the values.
+        /// </summary>
+        [Fact]
+        public void Exercise5()
+        {
+
+        }
+
+        /// <summary>
+        /// 6. Write a function 'recursiveReverse' that takes an array 
+        /// and uses recursion to return its contents in reverse
+        /// </summary>
+        [Fact]
+        public void Exercise6()
+        {
+
+        }
+    }
+}
