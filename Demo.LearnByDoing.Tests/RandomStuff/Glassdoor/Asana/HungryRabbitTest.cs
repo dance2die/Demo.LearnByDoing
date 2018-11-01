54299e2a280ddb7cb85ef9163d7d84c2aa0f7383
Sung Kim <sungmkim80@gmail.com>
2018-10-31 21:58:20 -0400

    Ugly code but all test cases pass!

--- Demo.LearnByDoing.Tests/RandomStuff/Glassdoor/Asana/HungryRabbitTest.cs
+++ Demo.LearnByDoing.Tests/RandomStuff/Glassdoor/Asana/HungryRabbitTest.cs
@@ -16,34 +16,45 @@ namespace Demo.LearnByDoing.Tests.RandomStuff.Glassdoor.Asana
 
         private int GetCarrotCount(int[,] m)
         {
+            var visited = new HashSet<Location>();
             var center = GetCenter(m);
             var max = center.Value;
-            var next = GetNext(center.X, center.Y, m);
+            visited.Add(center);
+
+            var next = GetNext(center.X, center.Y, m, visited);
 
             do
             {
                 max += next.Value;
-                next = GetNext(next.X, next.Y, m);
+                visited.Add(next);
+                next = GetNext(next.X, next.Y, m, visited);
             } while (next.Value != 0);
 
             return max;
         }
 
-        private Location GetNext(int x, int y, int[,] m)
+        private Location GetNext(int x, int y, int[,] m, HashSet<Location> visited)
         {
-            Location top = GetValueAt(x, y - 1, m);
-            Location right = GetValueAt(x + 1, y, m);
-            Location bottom = GetValueAt(x - 1, y, m);
+            Location top = GetValueAt(x - 1, y, m);
+            Location right = GetValueAt(x, y + 1, m);
+            Location bottom = GetValueAt(x + 1, y, m);
             Location left = GetValueAt(x, y - 1, m);
 
-            var locations = new[] {top, right, bottom, left};
+            var locations = new List<Location>();
+            if (!visited.Contains(top)) locations.Add(top);
+            if (!visited.Contains(right)) locations.Add(right);
+            if (!visited.Contains(bottom)) locations.Add(bottom);
+            if (!visited.Contains(left)) locations.Add(left);
+
+            if (locations.Count == 0) return new Location { Value = 0 };
+
             int max = locations.Max(_ => _.Value);
             return locations.FirstOrDefault(_ => _.Value == max);
         }
 
         private Location GetValueAt(int x, int y, int[,] m)
         {
-            var result = new Location();
+            var result = new Location { Value = 0 };
             if ((0 <= x && x < m.GetLength(0)) &&
                 (0 <= y && y < m.GetLength(1)))
             {
@@ -108,28 +119,6 @@ namespace Demo.LearnByDoing.Tests.RandomStuff.Glassdoor.Asana
         {
             yield return new object[]
             {
-                27,
-                new[,]
-                {
-                    {5, 7, 8, 6, 3},
-                    {0, 0, 7, 0, 4},
-                    {4, 7, 3, 4, 9},
-                    {3, 1, 0, 5, 8},
-                }
-            };
-            yield return new object[]
-            {
-                27,
-                new[,]
-                {
-                    {5, 7, 8, 6},
-                    {0, 0, 7, 0},
-                    {4, 6, 3, 4},
-                    {3, 1, 0, 5}
-                }
-            };
-            yield return new object[]
-            {
                 22,
                 new[,]
                 {
@@ -137,20 +126,43 @@ namespace Demo.LearnByDoing.Tests.RandomStuff.Glassdoor.Asana
                     {0, 0, 7},
                 }
             };
+
             yield return new object[]
             {
-                20,
-                new[,]
-                {
-                    {5, 7, 8, 6, 3},
-                }
+                    27,
+                    new[,]
+                    {
+                        {5, 7, 8, 6, 3},
+                        {0, 0, 7, 0, 4},
+                        {4, 7, 3, 4, 9},
+                        {3, 1, 0, 5, 8},
+                    }
+            };
+            yield return new object[]
+            {
+                    27,
+                    new[,]
+                    {
+                        {5, 7, 8, 6},
+                        {0, 0, 7, 0},
+                        {4, 6, 3, 4},
+                        {3, 1, 0, 5}
+                    }
+            };
+            yield return new object[]
+            {
+                    20,
+                    new[,]
+                    {
+                        {5, 7, 8, 6, 3},
+                    }
             };
         }
     }
 
-    class Location
+    struct Location
     {
-        public int Value { get; set; } = 0;
+        public int Value { get; set; }
         public int X { get; set; }
         public int Y { get; set; }
     }
