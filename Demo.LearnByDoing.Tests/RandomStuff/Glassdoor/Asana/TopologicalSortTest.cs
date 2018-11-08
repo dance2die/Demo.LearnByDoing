28ff62c67e08d00850a89642e2ce6a7e88aa7883
Sung Kim <sungmkim80@gmail.com>
2018-11-08 07:24:40 -0500

    Created a failing test

---
+++ Demo.LearnByDoing.Tests/RandomStuff/Glassdoor/Asana/TopologicalSortTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Demo.LearnByDoing.Tests.RandomStuff.Glassdoor.Asana
+{
+    public class TopologicalSortTest
+    {
+        [Fact]
+        public void TestHappyPath()
+        {
+            var graph = new Dictionary<char, List<char>>
+            {
+                {'a', new List<char> {'c'}},
+                {'b', new List<char> {'d', 'e'}},
+                {'c', new List<char> {'d'}},
+                {'d', new List<char> {'f'}},
+                {'e', new List<char> {'f'}},
+                {'f', new List<char> {'g'}},
+            };
+
+            var actual = GetTopologicallySorted(graph);
+            var expected = new [] { 'a', 'b', 'c', 'd', 'e', 'f', 'g' };
+            Assert.True(expected.SequenceEqual(actual));
+        }
+
+        private IEnumerable<char> GetTopologicallySorted(Dictionary<char, List<char>> g)
+        {
+            yield break;
+        }
+    }
+}
