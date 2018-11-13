1107bf6e14cbb386ba55acfe780c09cfd0fddfcb
Sung Kim <sungmkim80@gmail.com>
2018-11-13 07:16:55 -0500

    Added empty methods

--- Demo.LearnByDoing.Tests/RandomStuff/Glassdoor/Asana/KClosePointsToOriginTest.cs
+++ Demo.LearnByDoing.Tests/RandomStuff/Glassdoor/Asana/KClosePointsToOriginTest.cs
@@ -25,13 +25,42 @@ namespace Demo.LearnByDoing.Tests.RandomStuff.Glassdoor.Asana
                 (X: -2, Y: -3),
                 (X: 3, Y: 2),
             };
-            var expected = new[] {2, 1, 0};
-            var actual = GetKClosestPointsToOrigin(points);
+            var expected = new[]
+            {
+                (X: -2, Y: -3), 
+                (X: 0, Y: -2), 
+                (X: -1, Y: 0), 
+            };
+            const int k = 3;
+            var actual = GetKClosestPointsToOrigin(k, points);
 
             Assert.True(expected.SequenceEqual(actual));
         }
 
-        private IEnumerable<int> GetKClosestPointsToOrigin((int X, int Y)[] points)
+        private IEnumerable<(int, int)> GetKClosestPointsToOrigin(int k, (int X, int Y)[] points)
+        {
+            var pointToDistanceMap = BuildPointToDistanceMap(points);
+            var maxHeap = BuildMaxHeap(k, pointToDistanceMap);
+            for (int i = k; i < points.Length; i++)
+            {
+                // Replace the biggest with the current smallest
+                if (points[i] < maxHeap.Peek())
+                {
+                    maxHeap.Poll();
+                    maxHeap.Add(points[i]);
+                }
+            }
+
+            foreach (var point in maxHeap)
+                yield return point;
+        }
+
+        private Dictionary<(int, int), int> BuildPointToDistanceMap((int X, int Y)[] points)
+        {
+            throw new NotImplementedException();
+        }
+
+        private GenericMaxHeap<(int X, int Y)> BuildMaxHeap(int k, Dictionary<(int X, int Y), int> pointToDistanceMap)
         {
             throw new NotImplementedException();
         }
