cd90d6c0c82a3f4fb1c765a8522474b5dde84b36
Sung Kim <sungmkim80@gmail.com>
2018-11-24 15:25:18 -0500

    Turns out iterative version sorts in different way

--- Demo.LearnByDoing.Tests/RandomStuff/Glassdoor/Asana/TopologicalSortTest.cs
+++ Demo.LearnByDoing.Tests/RandomStuff/Glassdoor/Asana/TopologicalSortTest.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using Demo.LearnByDoing.Core;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Demo.LearnByDoing.Tests.RandomStuff.Glassdoor.Asana
 {
@@ -8,9 +11,10 @@ namespace Demo.LearnByDoing.Tests.RandomStuff.Glassdoor.Asana
     /// Tushar Roy
     /// https://www.youtube.com/watch?v=ddTC4Zovtbc
     /// </summary>
-    public class TopologicalSortTest
+    public class TopologicalSortTest : BaseTest
+
     {
-        public static IEnumerable<object[]> GetGraphs()
+        public static IEnumerable<object[]> GetInputForRecursion()
         {
             yield return new object[]
             {
@@ -29,15 +33,89 @@ namespace Demo.LearnByDoing.Tests.RandomStuff.Glassdoor.Asana
             };
         }
 
+        public static IEnumerable<object[]> GetInputForIteration()
+        {
+            yield return new object[]
+            {
+                new[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'},
+                new Dictionary<char, List<char>>
+                {
+                    {'a', new List<char> {'c'}},
+                    {'b', new List<char> {'d', 'c'}},
+                    {'c', new List<char> {'e'}},
+                    {'d', new List<char> {'f'}},
+                    {'e', new List<char> {'h', 'f'}},
+                    {'f', new List<char> {'g'}},
+                    {'g', new List<char>()},
+                    {'h', new List<char>()},
+                }
+            };
+        }
+
         [Theory]
-        [MemberData(nameof(GetGraphs))]
+        [MemberData(nameof(GetInputForRecursion))]
         public void TestRecursiveTopologicalSort(char[] expected, Dictionary<char, List<char>> graph)
         {
-            var actual = GetTopologicallySorted(graph);
+            var actual = GetTopologicallySortedRecursively(graph);
             Assert.True(expected.SequenceEqual(actual));
         }
 
-        private IEnumerable<char> GetTopologicallySorted(Dictionary<char, List<char>> g)
+        [Theory]
+        [MemberData(nameof(GetInputForIteration))]
+        public void TestIterativeTopologicalSort(char[] expected, Dictionary<char, List<char>> graph)
+        {
+            var actual = GetTopologicallySortedIteratively(graph).ToList();
+
+            foreach (char c in actual)
+            {
+                _output.WriteLine(c.ToString());
+            }
+
+            Assert.True(expected.SequenceEqual(actual));
+        }
+
+        private IEnumerable<char> GetTopologicallySortedIteratively(Dictionary<char, List<char>> g)
+        {
+            var visited = new HashSet<char>();
+            var sorted = new Queue<char>();
+
+            foreach (var node in g.Keys)
+            {
+                if (visited.Contains(node)) continue;
+
+                DepthFirstSearchNeighborsIteratively(g, node, visited, sorted);
+            }
+
+            return sorted.ToArray();
+        }
+
+        private void DepthFirstSearchNeighborsIteratively(
+            Dictionary<char, List<char>> g, char node,
+            HashSet<char> visited, Queue<char> sorted)
+        {
+            var stack = new Stack<char>();
+            stack.Push(node);
+            Func<bool> stackHasValue = () => stack.Count > 0;
+
+            while (stackHasValue())
+            {
+                var currentNode = stack.Pop();
+                visited.Add(currentNode);
+                if (!g.ContainsKey(currentNode)) continue;
+
+                foreach (var neighbor in g[node])
+                {
+                    if (visited.Contains(currentNode)) continue;
+
+                    stack.Push(neighbor);
+                    visited.Add(neighbor);
+                }
+            }
+
+            sorted.Enqueue(node);
+        }
+
+        private IEnumerable<char> GetTopologicallySortedRecursively(Dictionary<char, List<char>> g)
         {
             var visited = new HashSet<char>();
             var sorted = new Stack<char>();
@@ -46,13 +124,13 @@ namespace Demo.LearnByDoing.Tests.RandomStuff.Glassdoor.Asana
             {
                 if (visited.Contains(node)) continue;
 
-                DepthFirstSearchNeighbors(g, node, visited, sorted);
+                DepthFirstSearchNeighborsRecursively(g, node, visited, sorted);
             }
 
             return sorted;
         }
 
-        private void DepthFirstSearchNeighbors(Dictionary<char, List<char>> g, char node,
+        private void DepthFirstSearchNeighborsRecursively(Dictionary<char, List<char>> g, char node,
             HashSet<char> visited, Stack<char> sorted)
         {
             visited.Add(node);
@@ -62,10 +140,14 @@ namespace Demo.LearnByDoing.Tests.RandomStuff.Glassdoor.Asana
             {
                 if (visited.Contains(neighbor)) continue;
 
-                DepthFirstSearchNeighbors(g, neighbor, visited, sorted);
+                DepthFirstSearchNeighborsRecursively(g, neighbor, visited, sorted);
             }
 
             sorted.Push(node);
+        }
+
+        public TopologicalSortTest(ITestOutputHelper output) : base(output)
+        {
         }
     }
 }
